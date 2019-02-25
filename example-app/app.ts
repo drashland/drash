68ff8119ab52b92dffc400373b2390d09b33598a
Eric Crooks <eric.crooks@gmail.com>
2019-02-25 13:00:52 -0500

    Use hotfix branch

--- example-app/app.ts
+++ example-app/app.ts
@@ -1,4 +1,4 @@
-import Drash from "https://raw.githubusercontent.com/crookse/deno-drash/master/mod.ts"
+import Drash from "https://raw.githubusercontent.com/crookse/deno-drash/hotfix/cant-override-classes/mod.ts"
 
 import Response from "./response.ts";
 Drash.Http.Response = Response;
