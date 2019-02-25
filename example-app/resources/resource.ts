e816fd939bd831abf7f0e0f6a8c7df009b6356be
Eric Crooks <eric.crooks@gmail.com>
2019-02-25 13:10:13 -0500

    Use proper imports now

--- example-app/resources/resource.ts
+++ example-app/resources/resource.ts
@@ -1,4 +1,4 @@
-import Drash from "https://raw.githubusercontent.com/crookse/deno-drash/hotfix/cant-override-classes/mod.ts"
+import Drash from "https://deno.land/x/drash/mod.ts"
 
 export default class Resource extends Drash.Http.Resource {
   constructor(request, response) {
