7b1ee72af6c226c9f660424b8d72858d80110b78
Eric Crooks <eric.crooks@gmail.com>
2019-02-26 12:40:01 -0500

    Cover all content types

--- tests/unit/http/response_test.ts
+++ tests/unit/http/response_test.ts
@@ -4,16 +4,16 @@ let response = new members.Drash.Http.Response(members.mockRequest);
 response.body = "This is my body";
 let responseFormatted;
 
-members.test(async function Response_generateResponse_json() {
+members.test(async function Response_generateResponse_json_default() {
+  let expected = JSON.stringify({
+    status_code: 200,
+    status_message: "200 (OK)",
+    body: response.body
+  });
   responseFormatted = response.generateResponse();
-  members.assert.equal(
-    responseFormatted,
-    JSON.stringify({
-      status_code: 200,
-      status_message: "200 (OK)",
-      body: response.body
-    })
-  );
+  members.assert.equal(responseFormatted, expected);
+  responseFormatted = response.generateResponse();
+  members.assert.equal(responseFormatted, expected);
 });
 
 members.test(async function Response_generateResponse_html() {
@@ -37,6 +37,16 @@ members.test(async function Response_generateResponse_html() {
 
 members.test(async function Response_generateResponse_xml() {
   response.headers.set("Content-Type", "text/xml");
+  responseFormatted = response.generateResponse();
+  members.assert.equal(
+    responseFormatted,
+    `<response>
+  <statuscode>200</statuscode>
+  <statusmessage>200 (OK)</statusmessage>
+  <body>This is my body</body>
+</response>`
+  );
+  response.headers.set("Content-Type", "application/xml");
   responseFormatted = response.generateResponse();
   members.assert.equal(
     responseFormatted,
