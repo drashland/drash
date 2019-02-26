e0011b5b1bf43ef5941f9fa12a4a619a3e0d7fe2
Eric Crooks <eric.crooks@gmail.com>
2019-02-26 18:07:32 -0500

    Test Util.FileCreator

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,20 @@
 export class FileCreator {
-  static httpResources(pathToResources: string) {
+
+  static CONFIG = {
+    log_enabled: false
+  };
+
+  static log = function(message) {
+    if (FileCreator.CONFIG.log_enabled) {
+      console.log(message);
+    }
+  };
+
+  static httpResources(pathToResources: string, saveFileAs?: string) {
     let filename = `${pathToResources}/.drash_http_resources.ts`;
+    if (saveFileAs) {
+      filename = saveFileAs;
+    }
     const resources = Deno.readDirSync(pathToResources);
 
     let imports = "";
@@ -23,14 +37,10 @@ export class FileCreator {
 
     const encoder = new TextEncoder();
     const data = encoder.encode(imports + exports);
-    console.log(
-      `\nWriting HTTP resources file "${filename}" with the following content:`
-    );
-    console.log(imports + exports);
+    FileCreator.log(`\nWriting HTTP resources file "${filename}" with the following content:`);
+    FileCreator.log(imports + exports);
     Deno.writeFileSync(filename, data);
-    console.log(`\nFile ${filename} created!`);
-    console.log(
-      `\nMake sure to add the following to your project:\n    import resources from "${filename}";`
-    );
+    FileCreator.log(`\nFile ${filename} created!`);
+    FileCreator.log(`\nMake sure to add the following to your project:\n    import resources from "${filename}";`);
   }
 }
