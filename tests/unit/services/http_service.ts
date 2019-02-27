54e462ac268e060bfa7075ebf62412fd9ba4bd7b
Eric Crooks <eric.crooks@gmail.com>
2019-02-26 23:45:09 -0500

    prettier

--- tests/unit/services/http_service.ts
+++ tests/unit/services/http_service.ts
@@ -8,15 +8,30 @@ members.test(async function HttpService_hydrateHttpRequest() {
     false
   );
   request = members.Drash.Services.HttpService.hydrateHttpRequest(request);
-  members.assert.equal(
-    request.url_query_params,
-    {
-      what: "the",
-      ok: "then",
-      kthx: "bye",
-      empty: "",
-      cmon_now: undefined
-    }
-  );
+  members.assert.equal(request.url_query_params, {
+    what: "the",
+    ok: "then",
+    kthx: "bye",
+    empty: "",
+    cmon_now: undefined
+  });
 });
 
+members.test(async function HttpService_getHttpRequestUrlQueryParams() {
+  let request = members.mockRequest(
+    "/?what=the&ok=then&kthx=bye&empty=&cmon_now",
+    "get",
+    {},
+    false
+  );
+  let actual = members.Drash.Services.HttpService.getHttpRequestUrlQueryParams(
+    request
+  );
+  members.assert.equal(actual, {
+    what: "the",
+    ok: "then",
+    kthx: "bye",
+    empty: "",
+    cmon_now: undefined
+  });
+});
