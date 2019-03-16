2ff7a48d50af5b11e89d1686378819d81b4ab109
Eric Crooks <eric.crooks@gmail.com>
2019-03-16 09:38:25 -0400

    make use of just one input and output file for testing doc blocks

--- tests/unit/compilers/doc_blocks_to_json.ts
+++ tests/unit/compilers/doc_blocks_to_json.ts
@@ -1,25 +1,10 @@
 import members from "../../members.ts";
 
-// members.test(function DocBlocksToJson_compile() {
-//   const decoder = new TextDecoder();
-//   let rawContents = Deno.readFileSync(members.Drash.getEnvVar("DRASH_DIR_ROOT").value + "/tests/data/expected_compiler_output.json");
-//   let expected = JSON.parse(decoder.decode(rawContents));
-
-//   let compiler = new members.Drash.Compilers.DocBlocksToJson();
-
-//   let actual = compiler.compile([
-//     members.Drash.getEnvVar("DRASH_DIR_ROOT").value + "/tests/data/class_1.ts",
-//     members.Drash.getEnvVar("DRASH_DIR_ROOT").value + "/tests/data/class_2.ts",
-//   ]);
-
-//   members.assert.equal(expected, actual);
-// });
-
 members.test(function DocBlocksToJson_compile_params() {
   const decoder = new TextDecoder();
   let rawContents = Deno.readFileSync(
     members.Drash.getEnvVar("DRASH_DIR_ROOT").value +
-      "/tests/data/expected_compiler_output_params.json"
+      "/tests/data/doc_blocks_output.json"
   );
   let expected = JSON.parse(decoder.decode(rawContents));
 
@@ -27,7 +12,7 @@ members.test(function DocBlocksToJson_compile_params() {
 
   let actual = compiler.compile([
     members.Drash.getEnvVar("DRASH_DIR_ROOT").value +
-      "/tests/data/class_params.ts"
+      "/tests/data/doc_blocks_input.ts"
   ]);
 
   members.assert.equal(expected, actual);
