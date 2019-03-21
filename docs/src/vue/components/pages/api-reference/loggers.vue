26b6d2cc6d76e43473d5a7e5048846ede1d3cd73
Eric Crooks <eric.crooks@gmail.com>
2019-03-21 16:40:44 -0400

    fixing docs, compiler, and wondering wtf the image content type isn't working

---
+++ docs/src/vue/components/pages/api-reference/loggers.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+page-api-reference(:data="data")
+</template>
+
+<script>
+export const resource = {
+    paths: ["/api-reference/loggers/logger"],
+    meta: {
+        title: "Loggers",
+    }
+}
+
+export default {
+    data() {
+        return {
+            data: {
+                namespace: "Loggers",
+                class: this.$store.page_data.api_reference["Drash.Loggers"].Logger
+            }
+        };
+    }
+}
+</script>
