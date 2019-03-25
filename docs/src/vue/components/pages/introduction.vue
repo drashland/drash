49f19aacb17750430801e1d189b4fa2037e617f5
Eric Crooks <eric.crooks@gmail.com>
2019-03-25 16:49:46 -0400

    cleaning up intro page

--- docs/src/vue/components/pages/introduction.vue
+++ docs/src/vue/components/pages/introduction.vue
@@ -10,22 +10,21 @@ div
             div.col
                 heading-h2#what-is-drash(heading="What is Drash?")
                 p Drash is a modular web framework for <a href="https://deno.land" target="_BLANK">Deno</a> based on <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Identifying_resources_on_the_Web" target="_BLANK">HTTP resources</a> and <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Content_negotiation" target="_BLANK">content negotiation</a>.
-                p Drash helps you quickly build web apps, APIs, services, and whatever else you'd want to build using HTTP resources and content negotiation. Clients can make requests to any resource you create and can request any representation your resources allow (e.g., <code>application/json</code> format of the resource located at the <code>/user/1234</code> URI).
+                p Drash helps you quickly build web apps, APIs, services, and whatever else you'd want to build using HTTP resources and content negotiation. Clients make requests to the resources you create and can request any representation your resources allow (e.g., requesting the <code>application/json</code> format of the resource located at the <code>/user/1234</code> URI).
         hr
         div.row
             div.col
                 heading-h2#features(heading="Features")
                 h3 HTTP Resources
-                p Drash uses HTTP resources. It doesn't use controllers and it doesn't use <code>app.get('/', someHandler())</code>-like syntax. You create a resource class, define its URIs, and give it HTTP methods (e.g., <code>GET()</code>, <code>POST()</code>, <code>PUT()</code>, <code>DELETE()</code>, etc.).
+                p Drash uses HTTP resources. It doesn't use controllers and it doesn't use <code>app.get('/', someHandler())</code>-like syntax. You create a resource class, define its URIs, and give it the HTTP methods you want it to handle/allow (e.g., <code>GET()</code>, <code>POST()</code>, <code>PUT()</code>, <code>DELETE()</code>, etc.).
                 h3 Content Negotiation
-                p Drash is based on resources and you can't have true resources unless clients can request different representations of those resources through content negotiation. Drash ships with <code>application/json</code>, <code>text/html</code>, <code>application/xml</code>, and <code>text/xml</code> handling just to meet the needs of standard APIs and web apps. However, you can add more content types for your Drash server to handle.
-                p See the <a :href="$conf.base_url + '/#/tutorials/adding-content-types'">Adding Content Types</a> tutorial for more information.
+                p Drash is based on resources and you can't have true resources unless clients can request different representations of those resources through content negotiation. Out of the box, Drash's <code>Drash.Http.Response</code> class can generate the following representations for resources: <code>application/json</code>, <code>text/html</code>, <code>application/xml</code>, and <code>text/xml</code>. Getting the <code>Drash.Http.Response</code> class to handle more representations is easy. Read the <a :href="$conf.base_url + '/#/tutorials/adding-content-types'">Adding Content Types</a> tutorial for more information.
                 h3 Request Path Params (e.g., <code>/users/:id</code>)
                 p Resources can access their URI's path params via <code>this.request.path_params.some_param</code>--allowing you to build RESTful/ish APIs.
                 h3 Request URL Query Params (e.g., <code>/users?id=1234</code>)
-                p Can't have path params and not have request URL query params. Resources can access the request's URL query params via <code>this.request.url_query_params.some_param</code>.
+                p Resources can access the request's URL query params via <code>this.request.url_query_params.some_param</code>.
                 h3 Semantic Method Names
-                p If you want your resource class to allow <code>GET</code> requests, then give it a <code>GET()</code> method. If you want your resource class to allow POST requests, then give it a POST() method. If you don't want your resource class to allow DELETE requests, then don't give your resource class a DELETE() method. Pretty simple ideology and very semantic.
+                p If you want your resource class to allow <code>GET</code> requests, then give it a <code>GET()</code> method. If you want your resource class to allow <code>POST</code> requests, then give it a <code>POST()</code> method. If you don't want your resource class to allow <code>DELETE</code> requests, then don't give your resource class a <code>DELETE()</code> method. Pretty simple ideology and very semantic.
         hr
         div.row
             div.col
