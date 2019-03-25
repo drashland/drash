edab11116b90f7a6802689b518ef584630fc5960
Eric Crooks <eric.crooks@gmail.com>
2019-03-24 23:21:51 -0400

    new build

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -10,7 +10,7 @@ function getConf(envVars) {
     deno_version: envVars.deno_version.replace("deno: ", "Deno v")
       .replace("\nv8: ", ", V8 v")
       .replace("\ntypescript: ", ", and TypeScript v"),
-    latest_release: "v0.5.0",
+    latest_release: "v0.6.0",
     module_name: "Drash",
     module_namespace: "Drash",
     webpack_mode: envVars.environment
