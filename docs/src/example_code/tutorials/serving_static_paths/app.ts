c8dd8ea99a75086b865e0e305b574e194fabdc94
Eric Crooks <eric.crooks@gmail.com>
2019-03-28 12:38:32 -0400

    add DRASH_SERVER_DIRECTORY to dev.docs script

--- docs/src/example_code/tutorials/serving_static_paths/app.ts
+++ docs/src/example_code/tutorials/serving_static_paths/app.ts
@@ -1,7 +1,5 @@
 import Drash from "https://deno.land/x/drash/mod.ts";
 
-Deno.env().DRASH_SERVER_DIRECTORY = "/path/to/your/project"; // no trailing slash
-
 import HomeResource from "./home_resource.ts";
 
 let server = new Drash.Http.Server({
