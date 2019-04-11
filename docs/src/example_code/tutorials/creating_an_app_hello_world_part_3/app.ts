414f01e1cfe6c5569bafa4207f6536ed0c910426
Eric Crooks <eric.crooks@gmail.com>
2019-04-11 08:19:10 -0400

    remove test logging

--- docs/src/example_code/tutorials/creating_an_app_hello_world_part_3/app.ts
+++ docs/src/example_code/tutorials/creating_an_app_hello_world_part_3/app.ts
@@ -3,12 +3,7 @@ import Drash from "https://deno.land/x/drash/mod.ts";
 let server = new Drash.Http.Server({
   address: "localhost:8000",
   response_output: "text/html",
-  resources: [HomeResource],
-  logger: new Drash.Loggers.ConsoleLogger({
-    enabled: true,
-    level: "all",
-    tag_string: "{level} |"
-  })
+  resources: [HomeResource]
 });
 
 server.run();
