cd9b6415beef053cf72b4c5e1ded7ec354b93861
Eric Crooks <eric.crooks@gmail.com>
2019-04-13 12:34:46 -0400

    fixing content

--- docs/src/example_code/tutorials/adding_content_types/run.sh
+++ docs/src/example_code/tutorials/adding_content_types/run.sh
@@ -1,1 +1,1 @@
-deno /path/to/your/project/app.ts --allow-net
+deno /path/to/your/project/app.ts --allow-net --allow-env
--- docs/src/vue/components/pages/tutorials/adding_content_types.vue
+++ docs/src/vue/components/pages/tutorials/adding_content_types.vue
@@ -15,36 +15,23 @@ page-tutorial-default(:data="data" :hide_screenshot="true")
             li-download-source-code(:source_code_uri="$route.meta.source_code_uri")
     template(#steps)
         ol
-            li Create your HTTP resource class file.
-                ul
-                    li This class will handle <code>GET</code> requests to the <code>/</code> URI.
-                code-block(:data="data.example_code.home_resource")
-            li Create your new <code>Response</code> class.
-                ul
-                    li This class will be able to handle the following content types:
-                        ul
-                            li <code>text/html</code>
-                            li <code>application/json</code>
-                            li <code>application/pdf</code>
-                            li <code>application/xml</code>
-                            li <code>text/xml</code>
-                            li <code>text/plain</code>
-                code-block(:data="data.example_code.response")
             li Create your app file.
-                ul
-                    li This is the file that will be passed to the <code>deno</code> command in the terminal.
                 code-block(:data="data.example_code.app")
+            li Create your new <code>Response</code> class that will be used to replace <code>Drash.Http.Response</code>.
+                ul
+                    li Your new class will be able to handle the following content types: <code>text/html</code>, <code>application/json</code>, <code>application/pdf</code>, <code>application/xml</code>, <code>text/xml</code>, and <code>text/plain</code>.
+                    li Your new class only needs to override the <code>send()</code> method. You can override more members of the <code>Drash.Http.Response</code> class, but it's not necessary.
+                code-block(:data="data.example_code.response")
             li Run your app.
                 code-block(:data="data.example_code.run")
-            li Make the following <code>GET</code> and <code>POST</code> requests:
-                p.text--help I recommend using <a href="https://www.getpostman.com/" target="_BLANK">Postman</a> to make these requests. It's fast and versatile for web development.
+            li Make the following <code>GET</code> requests. I recommend using <a href="https://www.getpostman.com/" target="_BLANK">Postman</a> to make these requests. It's fast and versatile for web development.
                 ul
-                    li <code>localhost:8000?response_content_type=text/html</code>
-                    li <code>localhost:8000?response_content_type=application/json</code>
-                    li <code>localhost:8000?response_content_type=application/pdf</code>
-                    li <code>localhost:8000?response_content_type=application/xml</code>
-                    li <code>localhost:8000?response_content_type=text/xml</code>
-                    li <code>localhost:8000?response_content_type=text/plain</code>
+                    li <code>localhost:1337?response_content_type=text/html</code>
+                    li <code>localhost:1337?response_content_type=application/json</code>
+                    li <code>localhost:1337?response_content_type=application/pdf</code>
+                    li <code>localhost:1337?response_content_type=application/xml</code>
+                    li <code>localhost:1337?response_content_type=text/xml</code>
+                    li <code>localhost:1337?response_content_type=text/plain</code>
 </template>
 
 <script>
