6f9953a04c7447ca46cc2683a38ec2dd1a790067
Eric Crooks <eric.crooks@gmail.com>
2019-04-27 10:41:19 -0400

    Add What Is The Code Doing? section

--- docs/src/vue/components/pages/tutorials/serving_static_paths.vue
+++ docs/src/vue/components/pages/tutorials/serving_static_paths.vue
@@ -56,9 +56,36 @@ page-tutorial-default(
                     li Create your <code>style.css</code> file.
                         code-block(:data="data.example_code.public.style" title="/path/to/your/project/public/style.css")
                     li Run your app.
-                        ul
-                            li <code>--allow-read</code> is required so Deno can read the contents of your <code>style.css</code> file and serve its contents to the client.
                         code-block(:data="data.example_code.run")
+        div.row
+            div.col
+                hr
+                h2#what-is-the-code-doing What Is The Code Doing?
+                h3 <code>app.ts</code>
+                ol
+                    li Drash is imported so that all subsequent lines in the file have access to the <code>Drash</code> namespace.
+                    li The <code>HomeResource</code> class is imported to be used in the Drash server's <code>resources</code> config.
+                    li The Drash server is created.
+                        ul
+                            li The address for the server is set to <code>localhost:1337</code>. This means the full address to <code>HomeResource</code> is <code>localhost:1337/</code>.
+                            li The default response output is set to <code>text/html</code>. This means all responses from the server will default to the <code>text/html</code> MIME type. By default, Drash servers can handle the following types: <code>text/html</code>, <code>application/json</code>, <code>application/xml</code>, and <code>text/xml</code>. Drash servers can handle more content types, but you have to add that code. Read <a :href="$conf.base_url + '/#/tutorials/adding-content-types'">Adding Content Types</a> for more information.
+                            li The Drash server is given one resource: <code>HomeResource</code>.
+                    li The Drash server's <code>run()</code> method is invoked so that when <code>app.ts</code> is passed to the <code>deno</code> command in the terminal, the Drash server is started.
+                h3 <code>home_resource.ts</code>
+                ol
+                    li Drash is imported so that all subsequent lines in the file have access to the <code>Drash</code> namespace.
+                    li <code>HomeResource</code> contains a <code>GET()</code> method. This method will handle all <code>GET</code> requests to <code>HomeResource</code>. When a client makes a <code>GET</code> request to <code>HomeResource</code>, the response the client will receive is the HTML assigned to the <code>response</code> object's <code>body</code> property. All resources have access to the <code>response</code> object via <code>this.response</code>.
+                    li The HTML that <code>HomeResource</code> sets in the <code>response</code> object's <code>body</code> property has a <code>link</code> tag in the <code>head</code> tag that references <code>/public/style.css</code>. This means when the client receives the initial HTML response, the browser will make a subsequent request for the <code>style.css</code> file. When the Drash server receives the request for the <code>style.css</code> file, it will return the contents of the <code>style.css</code> file and set the response's <code>Content-Type</code> header to the correct MIME type (<code>text/css</code>), so the browser can properly handle the <code>style.css</code> file. The <code>style.css</code> sets <code>.my-text</code> to red.
+                    li The path (a.k.a. URI) that <code>HomeResource</code> opens up to clients is <code>/</code> (in the <code>static paths</code> property). This means clients can only access <code>HomeResource</code> through the <code>/</code> URI.
+                    li All resource classes MUST extend <code>Drash.Http.Resource</code>. You can create your own base resource class, but your base resource class has to extend <code>Drash.Http.Resource</code> before it can be extended further.
+                h3 <code>deno</code> (in the terminal)
+                ol
+                    li Deno runs <code>app.ts</code> and:
+                        ol
+                            li Allows network access via <code>--allow-net</code> flag.
+                            li Allows environment access via <code>--allow-env</code> flag.
+                        p The <code>--allow-net</code> flag is added so that the server will work.
+                        p The <code>--allow-env</code> flag is added because <code>Drash.Http.Response</code> objects require environment access if they're setup to serve static paths. Serving static paths isn't setup in this tutorial, but the <code>Drash.Http.Response</code> class compiles with the code that requires access to the environment. This means the <code>--allow-env</code> flag is always required.
         div.row
             div.col
                 hr
@@ -87,6 +114,7 @@ export default {
                         "Before You Get Started",
                         "Folder Structure End State",
                         "Steps",
+                        "What Is The Code Doing?",
                         "Screenshot"
                     ]
                 }
