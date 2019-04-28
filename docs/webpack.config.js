f0c995957c6bca7e80947064aec79d6e2a3e371a
Eric Crooks <eric.crooks@gmail.com>
2019-04-28 13:58:50 -0400

    bump version

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
 // Versions
-const latestRelease = "v0.8.2";
+const latestRelease = "v0.8.3";
 const denoVersionMin = "0.3.7";
 const denoVersionMax = "0.3.10";
 const denoStdVersion = "0.3.4";
