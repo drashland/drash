24eec1b88589acc7da743e510964a56eedbf2427
Eric Crooks <eric.crooks@gmail.com>
2019-06-23 18:05:03 -0400

    reference deno.land and not raw github

--- system.ts
+++ system.ts
@@ -1,7 +1,7 @@
 export {
   serve,
   ServerRequest
-} from "https://raw.githubusercontent.com/denoland/deno_std/v0.9.0/http/server.ts";
+} from "https://deno.land/std@v0.9.0/http/server.ts";
 
 export {
   STATUS_TEXT,
