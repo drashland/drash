c97282ecdda63450b90d50704adc61d3fc8a8d57
Eric Crooks <eric.crooks@gmail.com>
2019-06-23 10:01:44 -0400

    improve readability

--- docs/src/vue/components/pages/introduction.vue
+++ docs/src/vue/components/pages/introduction.vue
@@ -47,18 +47,25 @@ div
         div.row
             div.col
                 heading-h2#features(heading="Features")
-                h3 HTTP Resources
-                p Drash uses HTTP resources. It doesn't use controllers and it doesn't use <code>app.get('/', someHandler())</code>-like syntax. You create a resource class, define its URIs, and give it the HTTP methods you want it to handle/allow (e.g., <code>GET()</code>, <code>POST()</code>, <code>PUT()</code>, <code>DELETE()</code>, etc.).
-                h3 Content Negotiation
-                p Drash is based on resources and you can't have true resources unless clients can request different representations of those resources through content negotiation. Out of the box, Drash's <code>Drash.Http.Response</code> class can generate the following representations for resources: <code>application/json</code>, <code>text/html</code>, <code>application/xml</code>, and <code>text/xml</code>. Getting the <code>Drash.Http.Response</code> class to handle more representations is easy. Read the <a :href="$conf.base_url + '/#/tutorials/adding-content-types'">Adding Content Types</a> tutorial for more information.
-                h3 Request Path Params (e.g., <code>/users/:id</code>)
-                p Resources can access their URI's path params via <code>this.request.path_params.some_param</code>--allowing you to build RESTful/ish APIs.
-                h3 Request URL Query Params (e.g., <code>/users?id=1234</code>)
-                p Resources can access the request's URL query params via <code>this.request.url_query_params.some_param</code>.
-                h3 Request Body (e.g., <code>{"id":"1234"}</code>)
-                p Resources can access the request's body via <code>this.request.body_parsed.some_param</code>. Supported content types are <code>application/json</code> and <code>application/x-www-form-urlencoded</code>.
-                h3 Semantic Method Names
-                p If you want your resource class to allow <code>GET</code> requests, then give it a <code>GET()</code> method. If you want your resource class to allow <code>POST</code> requests, then give it a <code>POST()</code> method. If you don't want your resource class to allow <code>DELETE</code> requests, then don't give your resource class a <code>DELETE()</code> method. Pretty simple ideology and very semantic.
+                ul
+                    li 
+                        strong HTTP Resources
+                        p Drash uses HTTP resources. It doesn't use controllers and it doesn't use <code>app.get('/', someHandler())</code>-like syntax. You create a resource class, define its URIs, and give it the HTTP methods you want it to handle/allow (e.g., <code>GET()</code>, <code>POST()</code>, <code>PUT()</code>, <code>DELETE()</code>, etc.).
+                    li 
+                        strong Content Negotiation
+                        p Drash is based on resources and you can't have true resources unless clients can request different representations of those resources through content negotiation. Out of the box, Drash's <code>Drash.Http.Response</code> class can generate the following representations for resources: <code>application/json</code>, <code>text/html</code>, <code>application/xml</code>, and <code>text/xml</code>. Getting the <code>Drash.Http.Response</code> class to handle more representations is easy. Read the <a :href="$conf.base_url + '/#/tutorials/adding-content-types'">Adding Content Types</a> tutorial for more information.
+                    li  
+                        strong Request Path Params (e.g., <code>/users/:id</code>)
+                        p Resources can access their URI's path params via <code>this.request.path_params.some_param</code>--allowing you to build RESTful/ish APIs.
+                    li 
+                        strong Request URL Query Params (e.g., <code>/users?id=1234</code>)
+                        p Resources can access the request's URL query params via <code>this.request.url_query_params.some_param</code>.
+                    li 
+                        strong Request Body (e.g., <code>{"id":"1234"}</code>)
+                        p Resources can access the request's body via <code>this.request.body_parsed.some_param</code>. Supported content types are <code>application/json</code> and <code>application/x-www-form-urlencoded</code>.
+                    li 
+                        strong Semantic Method Names
+                        p If you want your resource class to allow <code>GET</code> requests, then give it a <code>GET()</code> method. If you want your resource class to allow <code>POST</code> requests, then give it a <code>POST()</code> method. If you don't want your resource class to allow <code>DELETE</code> requests, then don't give your resource class a <code>DELETE()</code> method. Pretty simple ideology and very semantic.
         hr
         div.row
             div.col
