2f54694142b80367a3228cb7e616e59766a54e34
Eric Crooks <eric.crooks@gmail.com>
2019-08-17 08:51:22 -0400

    bump deno_std to v0.15.0

--- deno_std.ts
+++ deno_std.ts
@@ -1,13 +1,13 @@
 export {
   serve,
   ServerRequest
-} from "https://raw.githubusercontent.com/denoland/deno_std/v0.12.0/http/server.ts";
+} from "https://raw.githubusercontent.com/denoland/deno_std/v0.15.0/http/server.ts";
 
 export {
   STATUS_TEXT,
   Status
-} from "https://deno.land/std@v0.12.0/http/http_status.ts";
+} from "https://deno.land/std@v0.15.0/http/http_status.ts";
 
 export {
   walkSync
-} from "https://deno.land/std@v0.12.0/fs/mod.ts";
+} from "https://deno.land/std@v0.15.0/fs/mod.ts";
