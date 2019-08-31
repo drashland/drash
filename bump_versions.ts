a2ab261d21001b082089391affa00982b7e3a7e6
Eric Crooks <eric.crooks@gmail.com>
2019-08-31 09:58:41 -0400

    fix regex

--- bump_versions.ts
+++ bump_versions.ts
@@ -2,21 +2,22 @@ const drash = "0.16.1";
 const deno = "0.16.0";
 const denoStd = "0.16.0";
 
-// Bump the versions in the README.md file
-const data = Deno.readFileSync("./README.md");
 const encoder = new TextEncoder();
 const decoder = new TextDecoder("utf-8");
-let text = decoder.decode(data);
+let text = "";
+
+// Bump the versions in the README.md file
+const readme = Deno.readFileSync("./README.md");
+text = decoder.decode(readme);
 text = text.replace(/deno-v([0-9]([0-9])?\.?)*/g, "deno-v" + deno);
 text = text.replace(/deno__std-v([0-9]([0-9])?\.?)*/g, "deno__std-v" + denoStd);
 Deno.writeFileSync("./README.md", encoder.encode(text));
 
 // Bump the versions in the docs/webpack.config.js file
-const data = Deno.readFileSync("./docs/webpack.config.js");
-const encoder = new TextEncoder();
-const decoder = new TextDecoder("utf-8");
-let text = decoder.decode(data);
-text = text.replace(/latestRelease.*([0-9]([0-9])?\.?)*/g, `latestRelease = "${drash}";`);
-text = text.replace(/denoVersion.*([0-9]([0-9])?\.?)*/g, `denoVersion = "${deno}";`);
-text = text.replace(/denoStdVersion.*([0-9]([0-9])?\.?)*/g, `denoStdVersion = "${denoStd}";`);
+const webpackConfig = Deno.readFileSync("./docs/webpack.config.js");
+text = decoder.decode(webpackConfig);
+text = text.replace(/latestRelease.*([0-9]([0-9])?\.?)*;/g, `latestRelease = "${drash}";`);
+text = text.replace(/denoVersion.*([0-9]([0-9])?\.?)*;/g, `denoVersion = "${deno}";`);
+text = text.replace(/denoStdVersion.*([0-9]([0-9])?\.?)*;/g, `denoStdVersion = "${denoStd}";`);
+console.log(text);
 Deno.writeFileSync("./README.md", encoder.encode(text));
