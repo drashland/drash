a0c793a4e4b02e52b163e0d680cafe829e51b989
Eric Crooks <eric.crooks@gmail.com>
2019-11-07 18:47:19 -0500

    reference deno std v0.23.0

--- deno_std.ts
+++ deno_std.ts
@@ -1,22 +1,22 @@
 export {
   serve,
   ServerRequest
-} from "https://raw.githubusercontent.com/denoland/deno_std/v0.20.0/http/server.ts";
+} from "https://deno.land/std@v0.23.0/http/server.ts";
 
 export {
   STATUS_TEXT,
   Status
-} from "https://deno.land/std@v0.20.0/http/http_status.ts";
+} from "https://deno.land/std@v0.23.0/http/http_status.ts";
 
 export {
   walkSync
-} from "https://deno.land/std@v0.20.0/fs/mod.ts";
+} from "https://deno.land/std@v0.23.0/fs/mod.ts";
 
 export {
   runTests,
   test
-} from "https://deno.land/std@v0.20.0/testing/mod.ts";
+} from "https://deno.land/std@v0.23.0/testing/mod.ts";
 
 export {
   assertEquals
-} from "https://deno.land/std@v0.20.0/testing/asserts.ts";
+} from "https://deno.land/std@v0.23.0/testing/asserts.ts";
