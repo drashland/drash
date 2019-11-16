f0d6b6e0e3af4c2c4e20128bbbf1d41480c772ce
Eric Crooks <eric.crooks@gmail.com>
2019-11-16 07:20:39 -0500

    [ft-#96-middleware] clean up test file; ready for implementation

--- tests/unit/http/server_test.ts
+++ tests/unit/http/server_test.ts
@@ -1,28 +1,5 @@
 import members from "../../members.ts";
 
-class HomeResource extends members.Drash.Http.Resource {
-  static paths = ["/"];
-  public GET() {
-    this.response.body = "got";
-    return this.response;
-  }
-  public POST() {
-    this.response.body = "got this";
-    return this.response;
-  }
-}
-
-class UserResource extends members.Drash.Http.Resource {
-  static paths = ["/user/:id", "/user/:id/"];
-  public GET() {
-    this.response.body = {};
-    return this.response;
-  }
-}
-
-// TODO(crookse)
-//     [ ] test request.body_parsed
-//     [ ] test favicon.ico request
 members.test(async function Server_handleHttpRequest_GET() {
   let server = new members.MockServer({
     resources: [HomeResource]
@@ -50,3 +27,59 @@ members.test(async function Server_handleHttpRequest_POST() {
       );
     });
 });
+
+members.test(async function Server_handleHttpRequest_middleware() {
+  let server = new members.MockServer({
+    middleware: [VerifyCsrfToken],
+    resources: [ResourceWithMiddleware]
+  });
+
+  server.handleHttpRequest(members.mockRequest("/users/1"))
+    .then((response) => {
+      members.assert.equal(
+        members.decoder.decode(response.body),
+        `{"status_code":401,"status_message":"Unauthorized","request":{"url":"/","method":"GET"},"body":null}`
+      );
+    });
+});
+
+////////////////////////////////////////////////////////////////////////////////
+// DATA ////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+class HomeResource extends members.Drash.Http.Resource {
+  static paths = ["/"];
+  public GET() {
+    this.response.body = "got";
+    return this.response;
+  }
+  public POST() {
+    this.response.body = "got this";
+    return this.response;
+  }
+}
+
+class VerifyCsrfToken extends members.Drash.Http.Middleware {
+  public run(request: any) {
+    if (!request.getHeaderVar('csrf_token')) {
+      throw new members.Drash.Exceptions.HttpException(400);
+    }
+  }
+}
+
+class ResourceWithMiddleware extends members.Drash.Http.Resource {
+  static paths = ["/user/:id", "/user/:id/"];
+  static middleware = ["Admin"];
+  public users = {
+    1: {
+      name: "Thor"
+    },
+    2: {
+      name: "Hulk"
+    },
+  };
+  public GET() {
+    this.response.body = {};
+    return this.response;
+  }
+}
