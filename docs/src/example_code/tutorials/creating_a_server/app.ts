3783059f8e57486ba94d818c4b87dec66dc93942
Eric Crooks <eric.crooks@gmail.com>
2019-11-17 09:30:29 -0500

    [ft-#96-middleware] editd wrong file (revert)

--- docs/src/example_code/tutorials/creating_a_server/app.ts
+++ docs/src/example_code/tutorials/creating_a_server/app.ts
@@ -1,14 +1,6 @@
 import Drash from "https://deno.land/x/drash/mod.ts";
 
-import HomeResource from "./resources/home_resource.ts";
-import VerifyToken from "./middleware/verify_token.ts";
+// Bootstrap your application
+import "./bootstrap.ts";
 
-let server = new Drash.Http.Server({
-  address: "localhost:8000",
-  logger: new Drash.Loggers.ConsoleLogger({enabled: true, level: "debug"}),
-  middleware: [VerifyToken],
-  resources: [HomeResource],
-  response_output: "application/json",
-});
-
-server.run();
+console.log(`\n    ${Drash.Members.MyThing.greet()}\n`);
