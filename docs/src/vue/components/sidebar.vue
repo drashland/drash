c0fb508382e637b90f83d0f1771e181fdbd01967
Eric Crooks <eric.crooks@gmail.com>
2019-11-17 07:45:17 -0500

    [ft-#96-middleware] rearrange sidebar items

--- docs/src/vue/components/sidebar.vue
+++ docs/src/vue/components/sidebar.vue
@@ -21,21 +21,18 @@ div.c-sidebar
                 a(:href="$conf.base_url + '/#/introduction#file-an-issue'") File An Issue
             a.heading-link Tutorials
             div.c-sidebar__menu-body
-                a Resources
-                div.l-submenu-1
-                    a(:href="$conf.base_url + '/#/tutorials/creating-a-resource'") Creating A Resource
-                    a(:href="$conf.base_url + '/#/tutorials/resource-lifecycle-hooks'") Lifecycle Hooks
+                a(:href="$conf.base_url + '/#/tutorials/creating-a-resource'") Creating A Resource
                 a(:href="$conf.base_url + '/#/tutorials/creating-a-server'") Creating A Server
                 a(:href="$conf.base_url + '/#/tutorials/serving-static-paths'") Serving Static Paths
                 a(:href="$conf.base_url + '/#/tutorials/adding-content-types'") Adding Content Types
                 a(:href="$conf.base_url + '/#/tutorials/content-negotiation'") Content Negotiation
+                a(:href="$conf.base_url + '/#/tutorials/adding-middleware'") Adding Middleware
+                a(:href="$conf.base_url + '/#/tutorials/adding-global-members'") Adding Global Members
                 a Logging
                 div.l-submenu-1
                     a(:href="$conf.base_url + '/#/tutorials/logging-to-the-terminal'") Logging To The Terminal
                     a(:href="$conf.base_url + '/#/tutorials/logging-to-files'") Logging To Files
                     router-link(href="/tutorials/logging-using-log-from-deno-std") Logging Using Log From deno_std
-                a(:href="$conf.base_url + '/#/tutorials/adding-global-members'") Adding Global Members
-                a(:href="$conf.base_url + '/#/tutorials/adding-middleware'") Adding Middleware
             a.heading-link Tutorials (Creating An App)
             div.c-sidebar__menu-body
                 a Hello World
