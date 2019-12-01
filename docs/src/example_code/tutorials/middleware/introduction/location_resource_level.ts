46a856246d9bd6f84330348327a48022549306cd
Eric Crooks <eric.crooks@gmail.com>
2019-11-30 20:04:39 -0500

    [ft-#96-middleware] finished with intro

--- docs/src/example_code/tutorials/middleware/introduction/location_resource_level.ts
+++ docs/src/example_code/tutorials/middleware/introduction/location_resource_level.ts
@@ -1,5 +1,7 @@
 class HomeResource extends Drash.Http.Resource {
+
   static paths = ["/"];
+
   static middleware = {
     before_request: [
       "MyFirstMiddleware"
@@ -8,23 +10,9 @@ class HomeResource extends Drash.Http.Resource {
       "MySecondMiddleware"
     ]
   };
+
   public GET() {
     this.response.body = "Hello";
     return this.response;
   }
 }
-
-let server = new Drash.Http.Server({
-  address: "localhost:1447",
-  middleware: {
-    resource_level: [
-      MyFirstMiddleware,
-      MySecondMiddleware,
-      MyThirdMiddleware
-    ]
-  },
-  resources: [
-    HomeResource
-  ],
-  response_output: "application/json",
-});
