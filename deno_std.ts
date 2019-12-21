308ad5e91bd0e167ac3c2bbceafe221a2961b556
Eric Crooks <eric.crooks@gmail.com>
2019-12-21 08:27:51 -0500

    [upgrade-deno] upgrade deno to v0.26.0

--- deno_std.ts
+++ deno_std.ts
@@ -1,22 +1,22 @@
 export {
   serve,
   ServerRequest
-} from "https://deno.land/std@v0.25.0/http/server.ts";
+} from "https://deno.land/std@v0.26.0/http/server.ts";
 
 export {
   STATUS_TEXT,
   Status
-} from "https://deno.land/std@v0.25.0/http/http_status.ts";
+} from "https://deno.land/std@v0.26.0/http/http_status.ts";
 
 export {
   walkSync
-} from "https://deno.land/std@v0.25.0/fs/mod.ts";
+} from "https://deno.land/std@v0.26.0/fs/mod.ts";
 
 export {
   runTests,
   test
-} from "https://deno.land/std@v0.25.0/testing/mod.ts";
+} from "https://deno.land/std@v0.26.0/testing/mod.ts";
 
 export {
   assertEquals
-} from "https://deno.land/std@v0.25.0/testing/asserts.ts";
+} from "https://deno.land/std@v0.26.0/testing/asserts.ts";
