af61c356ea2b9cb0563b3232d5898c2ba82609c5
Eric Crooks <eric.crooks@gmail.com>
2019-12-27 12:48:22 -0500

    [cb-#99] finish logging to files

---
+++ docs/src/example_code/tutorials/logging/logging_to_files/home_resource.ts
@@ -0,0 +1,20 @@
+import Drash from "https://deno.land/x/drash/mod.ts";
+
+export default class HomeResource extends Drash.Http.Resource {
+
+  static paths = ["/"];
+
+  public GET() {
+    this.server.logger.fatal("This is a FATAL log message.");
+    this.server.logger.error("This is an ERROR log message");
+    this.server.logger.warn("This is a WARN log message");
+    this.server.logger.info("This is an INFO log message");
+    this.server.logger.debug("This is a DEBUG log message");
+    this.server.logger.trace("This is a TRACE log message");
+
+    this.response.body = "GET request received! Also, check your server.log file to see the log messages written by this resource.";
+
+    return this.response;
+  }
+}
+
--- docs/src/vue/components/pages/tutorials/logging/logging_to_files.vue
+++ docs/src/vue/components/pages/tutorials/logging/logging_to_files.vue
@@ -1,75 +1,91 @@
 <template lang="pug">
-page-tutorial-logging(:data="data")
-    template(v-slot:steps)
-        ol
-            li Create your app file.
-                code-block(:data="example_code.app")
-            li Run your app.
-                code-block(:data="example_code.run")
-            li Check the terminal you used to start <code>app.ts</code>. The log messages in <code>HomeResource</code> will be written in the terminal. The other log messages come from the <code>Drash.Http.Server</code> class.
-                code-block-slotted
-                    template(v-slot:title) /path/to/your/project/server.log
-                    template(v-slot:code)
-                        | 2019-04-20 02:54:13.467Z | INFO |  Request received: GET /
-                        | 2019-04-20 02:54:13.468Z | DEBUG |  Calling HomeResource.GET() method.
-                        | 2019-04-20 02:54:13.468Z | FATAL |  This is a FATAL log message.
-                        | 2019-04-20 02:54:13.468Z | ERROR |  This is an ERROR log message
-                        | 2019-04-20 02:54:13.468Z | WARN |  This is a WARN log message
-                        | 2019-04-20 02:54:13.468Z | INFO |  This is an INFO log message
-                        | 2019-04-20 02:54:13.468Z | DEBUG |  This is a DEBUG log message
-                        | 2019-04-20 02:54:13.468Z | TRACE |  This is a TRACE log message
-                        | 2019-04-20 02:54:13.468Z | INFO |  Sending response. Content-Type: application/json. Status: 200 (OK).
-                        | 2019-04-20 02:54:13.485Z | DEBUG |  /favicon.ico requested.
-                        | 2019-04-20 02:54:13.485Z | DEBUG |  All future log messages for /favicon.ico will be muted.
-    template(v-slot:what-is-the-code-doing)
-        h3 <code>app.ts</code>
-        ol
-            li Drash is imported so that all subsequent lines in the file have access to the <code>Drash</code> namespace.
-            li A resource class named <code>HomeResource</code> is created.
-                ul
-                    li The path (a.k.a. URI) that <code>HomeResource</code> opens up to clients is <code>/</code> (in the <code>static paths</code> property). This means clients can only access <code>HomeResource</code> through the <code>/</code> URI.
-                    li <code>HomeResource</code> contains a <code>GET()</code> method. This method will handle all <code>GET</code> requests to <code>HomeResource</code>. When a client makes a <code>GET</code> request to <code>HomeResource</code>, log messages will be written to the <code>server.log</code> file.
-            li The Drash server is created.
-                ul
-                    li The address for the server is set to <code>localhost:1337</code>. This means the full address to <code>HomeResource</code> is <code>localhost:1337/</code>.
-                    li The default response output is set to <code>application/json</code>. This means all responses from the server will default to the <code>application/json</code> MIME type. By default, Drash servers can handle the following types: <code>text/html</code>, <code>application/json</code>, <code>application/xml</code>, and <code>text/xml</code>. Drash servers can handle more content types, but you have to add that code. Read <a :href="$conf.base_url + '/#/tutorials/adding-content-types'">Adding Content Types</a> for more information.
-                    li The Drash server is given one resource: <code>HomeResource</code>.
-                    li The server's <code>logger</code> config is used to add a <code>Drash.Loggers.FileLogger</code> object.
-                        ul
-                            li The logger is set to be enabled (this option exists so that message output can be turned off during unit tests) when the server runs; and the logger's log message level is <code>all</code>. This means all log messages will be displayed.
-                            li The logger is set to write to <code>./server.log</code>.
-                            li The logger's <code>tag_string</code> config is used to define a tag string that is prepended to the log message. All tags must be surrounded by the left <code>{</code> and the right <code>}</code>.
-                            li Each tag is mapped to a member in the logger's <code>tag_string_fns</code> config. For example, the <code>{datetime}</code> tag will get its data using the <code>datetime()</code> function. The <code>{level}</code> tag is a reserved tag and will always output the level of the current log message being written to the log. If a tag is defined in the <code>tag_string</code> config and can't be mapped to a member in the <code>tag_string_fns</code> config, then the tag will show as its written.
-            li The Drash server's <code>run()</code> method is invoked so that when <code>app.ts</code> is passed to the <code>deno</code> command in the terminal, the Drash server is started.
-        h3 <code>deno</code> (in the terminal)
-        ol
-            li Deno runs <code>app.ts</code> and:
-                ol
-                    li Allows network access via <code>--allow-net</code> flag.
-                    li Allows environment access via <code>--allow-env</code> flag.
-                    li Allows write access via <code>--allow-write</code> flag.
-                p The <code>--allow-net</code> flag is added so that the server will work.
-                p The <code>--allow-env</code> flag is added because <code>Drash.Http.Response</code> objects require environment access if they're setup to serve static paths. Serving static paths isn't setup in this tutorial, but the <code>Drash.Http.Response</code> class compiles with the code that requires access to the environment. This means the <code>--allow-env</code> flag is always required.
-                p The <code>--allow-write</code> flag is added to allow writing log messages to the <code>server.log</code> file.
+page-tutorial(
+  :toc="toc"
+)
+  div.row
+    div.col
+      hr
+      h2-hash Before You Get Started
+      p In this tutorial, you will learn how to log to the files using Drash's file logger.
+      p-download-source-code(:source_code_uri="$route.meta.source_code_uri")
+  div.row
+    div.col
+      hr
+      div-folder-structure-end-state(:code_block_data="example_code.folder_structure")
+  div.row
+    div.col
+      hr
+      h2-hash Steps
+      ol
+        li Create your resource file.
+          code-block(:data="example_code.home_resource")
+          p Before your resource sets the response's body, it will write log messages.
+        li Create your app file.
+          code-block(:data="example_code.app" line_highlight="11-12")
+          p Your server will write log messages based on the values of the highlighted code. In this tutorial, your server will write all log messages to <code>server.log</code>.
+  div.row
+    div.col
+      hr
+      h2-hash Verification
+      ol
+        li Run your app.
+          code-block-slotted
+            template(v-slot:title) Terminal
+            template(v-slot:code)
+              | deno --allow-net --allow-write app.ts
+          p-deno-flag-allow-net
+          p-deno-flag-allow-write
+        li Make a request using <code>curl</code> like below or go to <code>localhost:1447/</code> in your browser.
+          p
+            code-block-slotted
+              template(v-slot:title) Terminal
+              template(v-slot:code)
+                | curl localhost:1447/
+          p You should receive the following response:
+          p
+            code-block-slotted(language="javascript" :header="false")
+              template(v-slot:code)
+                | "GET request received! Also, check your server.log file to see the log messages written by this resource."
+        li Check your <code>server.log</code> file. The log messages in <code>HomeResource</code> will be written.
+          code-block-slotted(line_highlight="2,3,10")
+            template(v-slot:title) Terminal
+            template(v-slot:code)
+              | 2019-12-27 17:14:55.760Z | INFO | Request received: GET /
+              | 2019-12-27 17:14:55.762Z | DEBUG | [drash] Using `HomeResource` resource class to handle the request.
+              | 2019-12-27 17:14:55.762Z | DEBUG | [drash] Calling GET().
+              | 2019-12-27 17:14:55.762Z | FATAL | This is a FATAL log message.
+              | 2019-12-27 17:14:55.762Z | ERROR | This is an ERROR log message
+              | 2019-12-27 17:14:55.762Z | WARN | This is a WARN log message
+              | 2019-12-27 17:14:55.762Z | INFO | This is an INFO log message
+              | 2019-12-27 17:14:55.762Z | DEBUG | This is a DEBUG log message
+              | 2019-12-27 17:14:55.762Z | TRACE | This is a TRACE log message
+              | 2019-12-27 17:14:55.762Z | DEBUG | [drash] Sending response. 200.
+          p Drash's server has debug log messages for development purposes. They are highlighted above and prefixed with <code>[drash]</code>. All development related Drash log messages are prefixed with <code>[drash]</code> to help you identify which messages are from Drash.
 </template>
 
 <script>
 export const resource = {
-    paths: ["/tutorials/logging/logging-to-files"],
-    meta: {
-        title: "Logging: Logging To Files",
-        tutorial_title: "Logging To Files",
-        source_code_uri: "/tutorials/logging/logging_to_files"
-    }
+  paths: ["/tutorials/logging/logging-to-files"],
+  meta: {
+    title: "Logging To Files",
+    tutorial_title: "Logging To Files",
+    source_code_uri: "/tutorials/logging/logging_to_files"
+  }
 }
 
 export default {
-    data() {
-        return {
-            data: {
-                example_code: this.$app_data.example_code['/docs/src/example_code/tutorials/logging/logging_to_files']
-            }
-        };
-    },
+  data() {
+    return {
+      example_code: this.$app_data.example_code['/docs/src/example_code/tutorials/logging/logging_to_files'],
+      toc: {
+        items: [
+          "Before You Get Started",
+          "Folder Structure End State",
+          "Steps",
+          "Verification",
+        ]
+      }
+    };
+  },
 }
 </script>
