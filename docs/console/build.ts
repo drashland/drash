c8996049656bec4a61da82e47e04dfd8a8847500
Eric Crooks <eric.crooks@gmail.com>
2019-12-27 13:32:10 -0500

    stupid

--- docs/console/build.ts
+++ docs/console/build.ts
@@ -4,6 +4,6 @@ import * as ResponseService from "../src/response_service.ts";
 const drashDirRoot = Deno.env().DRASH_DIR_ROOT;
 
 ResponseService.compile(
-  `${drashDirRoot}/docs/src/templates/index.ejs`,
+  `${drashDirRoot}/docs/index.ejs`,
   `${drashDirRoot}/docs/index.html`
 );
