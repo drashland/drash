55a876a90c0a4cb0de5c979158a95c06795d24bb
PLAYER_4 <player.4.cod@gmail.com>
2020-03-02 15:31:47 -0500

    [upgrade-deno] bump

--- console/typescript/bump_versions.ts
+++ console/typescript/bump_versions.ts
@@ -1,4 +1,4 @@
-async function bumpVersions(fromV, toV) {
+async function bumpVersions(fromV: string, toV: string) {
   let data = new TextDecoder().decode(await Deno.readAll(await Deno.open("./deps.ts")));
   data = data.replace(new RegExp(fromV, "g"), toV);
   Deno.writeFileSync("./deps.ts", new TextEncoder().encode(data));
