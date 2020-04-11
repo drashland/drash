73ef1c63fa439c3459db1066317a595300fbba89
Edward Bebbington <edward.bebbington@intercity.technology>
2020-04-11 17:31:57 +0100

    Corrected conditional in server class

--- src/http/server.ts
+++ src/http/server.ts
@@ -144,8 +144,8 @@ export default class Server {
       });
     }
 
-    if (configs.views_renderer && !configs.views_path || configs.views_path && !configs.views_renderer) {
-      throw new Error('Property missing. Both the views_path and views_renderer must be passed in')
+    if (configs.views_renderer && !configs.views_path) {
+      throw new Error('Property missing. The views_path must be defined if the views_renderer is set')
     }
   }
 
