c7044701ec85c2e8158d7e124c594b3500e67436
Edward Bebbington <edward.bebbington@intercity.technology>
2020-04-13 23:19:51 +0100

    Fixed template_engine tests

--- example_app/template_engine_null_data_resource.ts
+++ example_app/template_engine_null_data_resource.ts
@@ -4,8 +4,8 @@ export default class TemplateEngineNullDataResource extends Drash.Http.Resource 
   static paths = ["/template-engine-null-data"];
   public GET() {
     this.response.headers.set("Content-Type", "text/html");
-    const engine = new Drash.Compilers.TemplateEngine();
-    this.response.body = engine.render("./user_null_data.html", null);
+    const engine = new Drash.Compilers.TemplateEngine("./");
+    this.response.body = engine.render("user_null_data.html", null);
     return this.response;
   }
 }
--- example_app/template_engine_resource.ts
+++ example_app/template_engine_resource.ts
@@ -4,9 +4,9 @@ export default class TemplateEngineResource extends Drash.Http.Resource {
   static paths = ["/template-engine"];
   public GET() {
     this.response.headers.set("Content-Type", "text/html");
-    const engine = new Drash.Compilers.TemplateEngine();
+    const engine = new Drash.Compilers.TemplateEngine("./");
     this.response.body = engine.render(
-      "./user.html",
+      "user.html",
       {
         skills: ["Shield Throwing", "Bashing", "Hammer Holding"],
         user: {
