36ac6b3763178ccf3cafcc76aab0266959409d5b
Edward Bebbington <edward.bebbington@intercity.technology>
2020-04-15 20:07:41 +0100

    Improved tests for request accept

--- example_app/request_accepts_resource.ts
+++ example_app/request_accepts_resource.ts
@@ -4,19 +4,19 @@ export default class RequestAcceptsResource extends Drash.Http.Resource {
   static paths = ["/request-accepts"];
 
   public GET() {
-    const matchedType = this.request.accepts("application/json");
-    if (!matchedType) {
-      this.response.body = JSON.stringify({
-        success: false,
-        message: 'This resource only accepts JSON'
-      })
-    }
-    if (matchedType === "application/json") {
-      this.response.body = JSON.stringify({
-        success: true,
-        message: 'Responding with the accepted content type'
-      })
-    }
+    const typeToRequest = this.request.getUrlQueryParam("typeToCheck");
+
+    let matchedType;
+    if (typeToRequest)
+      matchedType = this.request.accepts(typeToRequest);
+    else
+      matchedType = this.request.accepts(["text/html", "application/json"]);
+
+    if (!matchedType)
+      this.response.body = JSON.stringify({ success: false });
+    else
+      this.response.body = JSON.stringify({ success: true, message: matchedType });
+
     return this.response;
   }
 }
