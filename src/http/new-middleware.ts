e2e085dfc50220fcc7f1727b84e88bcbc41af029
Edward Bebbington <edward.bebbington@intercity.technology>
2020-04-16 18:48:42 +0100

    Improved upon middleware decorator

--- src/http/new-middleware.ts
+++ src/http/new-middleware.ts
@@ -21,16 +21,15 @@
 // THEN ON  REQUEST, USE THE RESOURCE NAME AND METHOD NEEDED TO SEARCH THRU THE MIDDLEWARES AND CALL THE REQUIRED MIDDLEWARE
 // METHOD (THAT METHOD WPULD NEED TO BE ADDED EG callMethodByName(methodName)
 
-let middlewares: any = []; // todo add type
-
-function registerMiddleware (resource: any, methodNameThatWantsTheMiddleware: string, middlewareToRun: string, methodToRun: Function) {
-    middlewares.push({
-        resource,
-        methodNameThatWantsTheMiddleware,
-        middlewareToRun,
-        methodToRun
-    })
+interface IMiddleware {
+    resource: any,
+    resourceName: string,
+    middlewareNameToRun: string,
+    resourceMethodNameThatIsUsingTheDecorator: string,
+    resourceMethodToRun: Function
 }
+
+let middlewares: IMiddleware[] = [];
 
 /**
  * @description
@@ -38,33 +37,49 @@ function registerMiddleware (resource: any, methodNameThatWantsTheMiddleware: st
  *     with the decorator.
  *
  * @param middlewareToRun
- *     @middleware('auth') // Says: 'run the auth middleware
+ *     @middleware('auth') // Says: 'run the auth middleware'
+ *     @middleware(['auth', 'logout']) // says 'run the auth then logout middleware'
  *
- * // TODO :: Maybe rename to register middleware?
  */
-function middlewareTest (middlewareToRun: string) {
+function middlewareTest (middlewareToRun: string|string[]) {
     return function (target: any, methodName: string, descriptor: PropertyDescriptor) {
         // Useful data
-        console.log({
-            middlewareToRun, // eg could be 'auth' if we want a route with the middleware of authorisation
-            target, // unsure
-            methodName, // the method the decorator was used with
-            descriptor, // holds a few key props, see below
-            className: target.constructor.name, // the name of the class that holds the method we added the decorator to
-            possibleClassObject: target.constructor, // untested, possibly the actual class object
-            methodWithDecorator: descriptor.value // the actual method we could call
+        // console.log({
+        //     middlewareToRun, // eg could be 'auth' if we want a route with the middleware of authorisation
+        //     target, // unsure
+        //     methodName, // the method the decorator was used with
+        //     descriptor, // holds a few key props, see below
+        //     className: target.constructor.name, // the name of the class that holds the method we added the decorator to
+        //     possibleClassObject: target.constructor, // untested, possibly the actual class object
+        //     methodWithDecorator: descriptor.value, // the actual method we could call
+        // })
+        if (typeof middlewareToRun === 'string') middlewareToRun = [middlewareToRun]
+        middlewareToRun.forEach(middlewareName => {
+            middlewares.push({
+                resource: target.constructor,
+                resourceName: target.constructor.name,
+                middlewareNameToRun: middlewareName,
+                resourceMethodNameThatIsUsingTheDecorator: methodName,
+                resourceMethodToRun: descriptor.value
+            })
         })
-        registerMiddleware(
-            target.constructor.name, // or target.constructor, depending on whether the class object is needed
-            methodName,
-            middlewareToRun,
-            descriptor.value
-        )
     }
 }
 
-function getMiddlewares () {
-    return middlewares
+function getMiddlewares (resourceName: string, methodName: string) {
+    let foundMiddlewares: IMiddleware[] = []
+    middlewares.forEach(middleware => {
+        console.log('looping through the middlewares')
+        console.log(middleware)
+        if (
+            middleware.resourceName === resourceName
+            &&
+            middleware.resourceMethodNameThatIsUsingTheDecorator === methodName.toUpperCase()
+        ) {
+            foundMiddlewares.push(middleware)
+        }
+    })
+    return foundMiddlewares
 }
 
 export {middlewareTest, getMiddlewares}
