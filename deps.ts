c41df99818bb416172b366cf12ec63cbe6f39559
Eric Crooks <eric.crooks@gmail.com>
2020-04-16 11:59:37 -0400

    [upgrade-deno] upgrade to deno v0.41.0

--- deps.ts
+++ deps.ts
@@ -1,39 +1,39 @@
 export {
   ServerRequest,
   serve
-} from "https://deno.land/std@v0.39.0/http/server.ts";
+} from "https://deno.land/std@v0.41.0/http/server.ts";
 
 export {
   STATUS_TEXT,
   Status
-} from "https://deno.land/std@v0.39.0/http/http_status.ts";
+} from "https://deno.land/std@v0.41.0/http/http_status.ts";
 
 export {
   walkSync
-} from "https://deno.land/std@v0.39.0/fs/mod.ts";
+} from "https://deno.land/std@v0.41.0/fs/mod.ts";
 
 export {
   assertEquals,
   assertThrows
-} from "https://deno.land/std@v0.39.0/testing/asserts.ts";
+} from "https://deno.land/std@v0.41.0/testing/asserts.ts";
 
 export {
   BufReader,
   ReadLineResult
-} from "https://deno.land/std@v0.39.0/io/bufio.ts";
+} from "https://deno.land/std@v0.41.0/io/bufio.ts";
 
 export {
   StringReader
-} from "https://deno.land/std@v0.39.0/io/readers.ts";
+} from "https://deno.land/std@v0.41.0/io/readers.ts";
 
 export {
   FormFile,
   MultipartReader
-} from "https://deno.land/std@v0.39.0/mime/multipart.ts";
+} from "https://deno.land/std@v0.41.0/mime/multipart.ts";
 
 export {
   Cookie,
   delCookie,
   getCookies,
   setCookie
-} from "https://deno.land/std@v0.39.0/http/cookie.ts";
+} from "https://deno.land/std@v0.41.0/http/cookie.ts";
