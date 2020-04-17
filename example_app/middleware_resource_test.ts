50a2d91b2aa7ed97e1bee707ff4ce20a5fc2316f
Eric Crooks <eric.crooks@gmail.com>
2020-04-17 08:33:25 -0400

    [issue-#176-decorators] comments

--- example_app/middleware_resource_test.ts
+++ example_app/middleware_resource_test.ts
@@ -2,14 +2,15 @@ import members from "../tests/members.ts";
 members.test("MiddlewareResource", async () => {
   let response;
 
+  // Assert that passing in the token works
   response = await members.fetch.get("http://localhost:1667/middleware", {
     headers: {
       "token": "zeToken",
     },
   });
-
   await members.assert.equals(await response.text(), '"GET request received!"');
 
+  // Assert that not passing in the token doesn't work
   response = await members.fetch.get("http://localhost:1667/middleware");
   await members.assert.equals(await response.text(), '"No token, dude."');
 });
