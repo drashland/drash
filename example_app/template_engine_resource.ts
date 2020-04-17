8d4a81dc71789202fd5c45a5b351c4e24f20b0b1
Eric Crooks <eric.crooks@gmail.com>
2020-04-17 08:30:06 -0400

    fix app tests

--- example_app/template_engine_resource.ts
+++ example_app/template_engine_resource.ts
@@ -1,4 +1,4 @@
-import Drash from "../mod.ts";
+import { Drash } from "../mod.ts";
 
 export default class TemplateEngineResource extends Drash.Http.Resource {
   static paths = ["/template-engine"];
--- example_app/users_resource.ts
+++ example_app/users_resource.ts
@@ -1,4 +1,4 @@
-import Drash from "../mod.ts";
+import { Drash } from "../mod.ts";
 
 export default class UsersResource extends Drash.Http.Resource {
   static paths = ["/users", "/users/:id"];
