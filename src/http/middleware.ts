13e05132684b15af29ee5c51b624742926808f61
Guergeiro <business@brenosalles.com>
2020-05-02 22:39:43 +0100

    Removed the need for extra tsconfig.json params for middleware

--- src/http/middleware.ts
+++ src/http/middleware.ts
@@ -6,9 +6,7 @@ import { Drash } from "../../mod.ts";
  * @param server Contains the instance of the server.
  * @param response Contains the instance of the response.
  */
-export type MiddlewareFunction =
-  | ((request: any, response: Drash.Http.Response) => Promise<void>)
-  | ((request: any, response: Drash.Http.Response) => void);
+export type MiddlewareFunction = ((request: any, response: Drash.Http.Response) => Promise<void>) | ((request: any, response: Drash.Http.Response) => void);
 
 /**
  * @type MiddlewareType
@@ -23,34 +21,36 @@ export type MiddlewareFunction =
  *
  */
 export type MiddlewareType = {
-  before_request?: MiddlewareFunction[];
-  after_request?: MiddlewareFunction[];
+    before_request?: MiddlewareFunction[];
+    after_request?: MiddlewareFunction[];
 };
 
 /**
  * Executes the middleware
- * 
+ *
  * @param middlewares Contains middlewares to be executed
  */
 export function Middleware(middlewares: MiddlewareType) {
-  return function (this: any, ...args: any) {
-    switch (args.length) {
-      case 1:
-        // Class decorator
-        return ClassMiddleware(middlewares).apply(this, args);
-      case 2:
-        // Property decorator
-        break;
-      case 3:
-        if (typeof args[2] == "number") {
-          // Parameter decorator
-          break;
+    return function (...args: any[]) {
+        switch (args.length) {
+            case 1:
+                // Class decorator
+                // @ts-ignore
+                return ClassMiddleware(middlewares).apply(this, args);
+            case 2:
+                // Property decorator
+                break;
+            case 3:
+                if (typeof args[2] == "number") {
+                    // Parameter decorator
+                    break;
+                }
+                // @ts-ignore
+                return MethodMiddleware(middlewares).apply(this, args);
+            default:
+                throw new Error("Not a valid decorator");
         }
-        return MethodMiddleware(middlewares).apply(this, args);
-      default:
-        throw new Error("Not a valid decorator");
-    }
-  };
+    };
 }
 
 /**
@@ -59,35 +59,31 @@ export function Middleware(middlewares: MiddlewareType) {
  * @param middlewares Contains all middleware to be run
  */
 export function MethodMiddleware(middlewares: MiddlewareType) {
-  return function (
-    target: any,
-    propertyKey: string,
-    descriptor: PropertyDescriptor,
-  ) {
-    const originalFunction = descriptor.value;
-    descriptor.value = async function (...args: any[]) {
-      const { request, response } = Object.getOwnPropertyDescriptors(this);
-      // Execute before_request Middleware if exist
-      if (middlewares.before_request != null) {
-        for (const middleware of middlewares.before_request) {
-          await middleware(request.value, response.value);
-        }
-      }
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        const originalFunction = descriptor.value;
+        descriptor.value = async function (...args: any[]) {
+            const { request, response } = Object.getOwnPropertyDescriptors(this);
+            // Execute before_request Middleware if exist
+            if (middlewares.before_request != null) {
+                for (const middleware of middlewares.before_request) {
+                    await middleware(request.value, response.value);
+                }
+            }
 
-      // Execute function
-      const result = originalFunction.apply(this, args);
+            // Execute function
+            const result = originalFunction.apply(this, args);
 
-      // Execute after_request Middleware if exist
-      if (middlewares.after_request != null) {
-        for (const middleware of middlewares.after_request) {
-          await middleware(request.value, result);
-        }
-      }
-      return result;
+            // Execute after_request Middleware if exist
+            if (middlewares.after_request != null) {
+                for (const middleware of middlewares.after_request) {
+                    await middleware(request.value, result);
+                }
+            }
+            return result;
+        };
+
+        return descriptor;
     };
-
-    return descriptor;
-  };
 }
 
 /**
@@ -96,25 +92,25 @@ export function MethodMiddleware(middlewares: MiddlewareType) {
  * @param middlewares Contains all middleware to be run
  */
 export function ClassMiddleware(middlewares: MiddlewareType) {
-  return function <T extends { new (...args: any[]): {} }>(constr: T) {
-    return class extends constr {
-      constructor(...args: any[]) {
-        const request = args[0];
-        const response = args[1];
-        // Execute before_request Middleware if exist
-        if (middlewares.before_request != null) {
-          for (const middleware of middlewares.before_request) {
-            middleware(request, response);
-          }
-        }
-        super(...args);
-        // Execute after_request Middleware if exist
-        if (middlewares.after_request != null) {
-          for (const middleware of middlewares.after_request) {
-            middleware(request, response);
-          }
-        }
-      }
+    return function <T extends { new (...args: any[]): {} }>(constr: T) {
+        return class extends constr {
+            constructor(...args: any[]) {
+                const request = args[0];
+                const response = args[1];
+                // Execute before_request Middleware if exist
+                if (middlewares.before_request != null) {
+                    for (const middleware of middlewares.before_request) {
+                        middleware(request, response);
+                    }
+                }
+                super(...args);
+                // Execute after_request Middleware if exist
+                if (middlewares.after_request != null) {
+                    for (const middleware of middlewares.after_request) {
+                        middleware(request, response);
+                    }
+                }
+            }
+        };
     };
-  };
 }
