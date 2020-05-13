8505ecf5193cb41c8389f682ed38f9b642e768e1
Eric Crooks <eric.crooks@gmail.com>
2020-05-13 19:17:30 -0400

    [upgrade-deno] bump versions

--- deps.ts
+++ deps.ts
@@ -5,35 +5,35 @@ export {
   ServerRequest,
   serve,
   serveTLS,
-} from "https://deno.land/std@v1.0.0-rc1/http/server.ts";
+} from "https://deno.land/std@v1.0.0/http/server.ts";
 
 export {
   STATUS_TEXT,
   Status,
-} from "https://deno.land/std@v1.0.0-rc1/http/http_status.ts";
+} from "https://deno.land/std@v1.0.0/http/http_status.ts";
 
 export {
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@v1.0.0-rc1/testing/asserts.ts";
+} from "https://deno.land/std@v1.0.0/testing/asserts.ts";
 
 export {
   BufReader,
   ReadLineResult,
-} from "https://deno.land/std@v1.0.0-rc1/io/bufio.ts";
+} from "https://deno.land/std@v1.0.0/io/bufio.ts";
 
 export {
   StringReader,
-} from "https://deno.land/std@v1.0.0-rc1/io/readers.ts";
+} from "https://deno.land/std@v1.0.0/io/readers.ts";
 
 export {
   FormFile,
   MultipartReader,
-} from "https://deno.land/std@v1.0.0-rc1/mime/multipart.ts";
+} from "https://deno.land/std@v1.0.0/mime/multipart.ts";
 
 export {
   Cookie,
   delCookie,
   getCookies,
   setCookie,
-} from "https://deno.land/std@v1.0.0-rc1/http/cookie.ts";
+} from "https://deno.land/std@v1.0.0/http/cookie.ts";
