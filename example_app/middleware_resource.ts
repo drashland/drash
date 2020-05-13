c966076c4137536011d932b1c2af98ca71a47ea1
Eric Crooks <eric.crooks@gmail.com>
2020-05-13 19:55:43 -0400

    [upgrade-deno] update resource

--- example_app/middleware_resource.ts
+++ example_app/middleware_resource.ts
@@ -2,6 +2,7 @@ import { Drash } from "../mod.ts";
 
 export default class MiddlewareResource extends Drash.Http.Resource {
   static paths = ["/middleware"];
+
   public GET() {
     this.response.body = "GET request received!";
     return this.response;
