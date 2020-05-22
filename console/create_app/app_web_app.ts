6a3bf13e27333205ba4ad191f2db2ca07b12dc65
ebebbington <EdwardSBebbington@hotmail.com>
2020-05-22 13:27:25 +0100

    Added boilerplate files for create_app script

---
+++ console/create_app/app_web_app.ts
@@ -0,0 +1,23 @@
+import { Drash } from "./deps.ts"
+import HomeResource from "./resources//home_resource.ts";
+
+const server = new Drash.Http.Server({
+    directory: Deno.realPathSync("./"),
+    response_output: "text/html",
+    logger: new Drash.CoreLoggers.ConsoleLogger({
+        enabled: false,
+        level: "debug",
+    }),
+    resources: [
+        HomeResource,
+    ],
+    static_paths: ["/public"],
+    views_path: "./public/views",
+});
+
+await server.run({
+    hostname: "localhost",
+    port: 1667,
+});
+
+console.log(`Server listening: http://${server.hostname}:${server.port}`);
---
+++ console/create_app/deps.ts
@@ -0,0 +1,2 @@
+export { Drash } from "https://deno.land/x/drash@v0.50.0/mod.ts"
+export { assertEquals } from "https://deno.land/std@v0.50.0/testing/asserts.ts";
