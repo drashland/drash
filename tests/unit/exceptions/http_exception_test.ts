6959ca478a0a5b3a3fa492aa43f82b53e4a1085d
Eric Crooks <eric.crooks@gmail.com>
2020-06-09 17:39:28 -0400

    [unit-tests] update exceptions tests

--- tests/unit/exceptions/http_exception_test.ts
+++ tests/unit/exceptions/http_exception_test.ts
@@ -1,11 +1,18 @@
 import members from "../../members.ts";
+import { Drash } from "../../../mod.ts";
 
-members.test("http_exception_test.ts | Exceptions.HttpException(405)", () => {
-  let actual = new members.Drash.Exceptions.HttpException(405);
-  members.assert.equal(actual.code, 405);
-});
+members.testSuite("exceptions/http_exception_test.ts", () => {
 
-members.test("http_exception_test.ts | Exceptions.HttpException(418)", () => {
-  let actual = new members.Drash.Exceptions.HttpException(418);
-  members.assert.equal(actual.code, 418);
+  members.test("Exceptions.HttpException(405) shows correct code and message", () => {
+    const actual = new Drash.Exceptions.HttpException(405, "HAAAAAA. Not allowed.");
+    members.assertEquals(actual.code, 405);
+    members.assertEquals(actual.message, "HAAAAAA. Not allowed.");
+  });
+
+  members.test("Exceptions.HttpException(418) shows correct code and message", () => {
+    const actual = new Drash.Exceptions.HttpException(418, "Yo... 418, bro.");
+    members.assertEquals(actual.code, 418);
+    members.assertEquals(actual.message, "Yo... 418, bro.");
+  });
+
 });
