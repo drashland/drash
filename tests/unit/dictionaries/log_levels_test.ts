bfd95420d50152702021be804bd8bc39cec43537
Eric Crooks <eric.crooks@gmail.com>
2020-06-09 17:39:12 -0400

    [unit-tests] update dictionaries

--- tests/unit/dictionaries/log_levels_test.ts
+++ tests/unit/dictionaries/log_levels_test.ts
@@ -1,46 +1,51 @@
 import members from "../../members.ts";
+import { Drash } from "../../../mod.ts";
 
-let expected = {
-  all: {
-    rank: 7,
-    name: "All",
-  },
-  trace: {
-    rank: 6,
-    name: "Trace",
-  },
-  debug: {
-    rank: 5,
-    name: "Debug",
-  },
-  info: {
-    rank: 4,
-    name: "Info",
-  },
-  warn: {
-    rank: 3,
-    name: "Warn",
-  },
-  error: {
-    rank: 2,
-    name: "Error",
-  },
-  fatal: {
-    rank: 1,
-    name: "Fatal",
-  },
-  off: {
-    rank: 0,
-    name: "Off",
-  },
-};
+members.testSuite("dictionaries/log_levels_test.ts", () => {
 
-let actual: any = {};
+  let expected = {
+    all: {
+      rank: 7,
+      name: "All",
+    },
+    trace: {
+      rank: 6,
+      name: "Trace",
+    },
+    debug: {
+      rank: 5,
+      name: "Debug",
+    },
+    info: {
+      rank: 4,
+      name: "Info",
+    },
+    warn: {
+      rank: 3,
+      name: "Warn",
+    },
+    error: {
+      rank: 2,
+      name: "Error",
+    },
+    fatal: {
+      rank: 1,
+      name: "Fatal",
+    },
+    off: {
+      rank: 0,
+      name: "Off",
+    },
+  };
 
-for (let logLevel in expected) {
-  actual[logLevel] = members.Drash.Dictionaries.LogLevels.get(logLevel);
-}
+  let actual: any = {};
 
-members.test("log_levels_test.ts | LogLevels", () => {
-  members.assert.equal(actual, expected);
+  for (let logLevel in expected) {
+    actual[logLevel] = Drash.Dictionaries.LogLevels.get(logLevel);
+  }
+
+  members.test("LogLevels", () => {
+    members.assertEquals(actual, expected);
+  });
+
 });
