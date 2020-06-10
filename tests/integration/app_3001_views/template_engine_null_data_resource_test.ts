0828947ba2ec16b549df2ec6132190365ce86190
Eric Crooks <eric.crooks@gmail.com>
2020-06-10 17:41:33 -0400

    [unit-tests] get expected based on os

--- tests/integration/app_3001_views/template_engine_null_data_resource_test.ts
+++ tests/integration/app_3001_views/template_engine_null_data_resource_test.ts
@@ -1,6 +1,6 @@
 import members from "../../members.ts";
 
-function getOsExpected() {
+function getExpected() {
   if (Deno.build.os == "windows") {
     return `<!DOCTYPE html>  <html class=\"h-full w-full\">    <head>      <meta charset=\"utf-8\"/>      <meta name=\"viewport\" content=\"width=device-width, minimum-scale=1.0, user-scalable=no\"/>      <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css\">      <title>Skills</title>    </head>    <body>          <div style=\"max-width: 640px; margin: 50px auto;\">    <h1 class=\"text-5xl\">Thor</h1>  </div>      </body>  </html>  `;
   }
