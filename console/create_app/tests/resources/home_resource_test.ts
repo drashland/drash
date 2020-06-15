f86cbe4de2ee506f89894102581277b00983e804
Eric Crooks <eric.crooks@gmail.com>
2020-06-14 22:41:18 -0400

    fix boilerplate test

--- console/create_app/tests/resources/home_resource_test.ts
+++ console/create_app/tests/resources/home_resource_test.ts
@@ -2,16 +2,32 @@ import { Drash } from "../../deps.ts";
 import { assertEquals } from "../../deps.ts";
 import HomeResource from "../../resources/home_resource.ts";
 
+const server = new Drash.Http.Server({
+  response_output: "application/json",
+  resources: [
+    HomeResource,
+  ],
+});
+
+server.run({
+  hostname: "localhost",
+  port: 1557,
+});
+console.log(`Server listening: http://${server.hostname}:${server.port}`);
+
 Deno.test({
   name: "HomeResource - GET /",
   async fn(): Promise<void> {
-    const server = new Drash.Http.Server({
-      address: "localhost:1557",
-      resources: [HomeResource],
-    });
-    await server.run();
     const response = await fetch("http://localhost:1557");
     assertEquals(response.status, 200);
-    server.close();
   },
+});
+
+Deno.test({
+  name: "Stop the server",
+  async fn() {
+    await server.close();
+  },
+  sanitizeResources: false,
+  sanitizeOps: false,
 });
