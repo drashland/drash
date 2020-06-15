01a27159ae7aba0e6bd6252822581905b2e9bc87
Eric Crooks <eric.crooks@gmail.com>
2020-06-14 22:49:07 -0400

    deno fmt

--- console/create_app/tests/resources/home_resource_test.ts
+++ console/create_app/tests/resources/home_resource_test.ts
@@ -16,13 +16,16 @@ server.run({
 console.log(`Server listening: http://${server.hostname}:${server.port}`);
 
 Deno.test("HomeResource - GET /", async () => {
-    const response = await fetch("http://localhost:1557", {
-      method: "POST"
-    });
-    assertEquals(response.status, 200);
-    assertEquals(await response.json(), JSON.stringify({
-      message: "Not implemented"
-    }));
+  const response = await fetch("http://localhost:1557", {
+    method: "POST",
+  });
+  assertEquals(response.status, 200);
+  assertEquals(
+    await response.json(),
+    JSON.stringify({
+      message: "Not implemented",
+    }),
+  );
 });
 
 Deno.test({
