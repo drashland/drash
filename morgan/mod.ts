1d02809276c2a722674dced0e11e3649b39b4505
Eric Crooks <eric.crooks@gmail.com>
2020-07-12 09:06:44 -0400

    add response_time config

--- morgan/mod.ts
+++ morgan/mod.ts
@@ -1,12 +1,16 @@
 import { Drash } from "../deps.ts";
 
+interface IMorganConfigs extends Drash.Interfaces.LoggerConfigs {
+  response_time?: boolean;
+}
+
 /**
  * A logger middleware inspired by https://www.npmjs.com/package/morgan.
  *
  * @param configs - See https://doc.deno.land/https/deno.land/x/drash/src/interfaces/logger_configs.ts
  */
 export function Morgan(
-  configs?: Drash.Interfaces.LoggerConfigs
+  configs?: IMorganConfigs
 ) {
   const defaultConfigs = {
     enabled: true,
@@ -46,33 +50,43 @@ export function Morgan(
 
   Morgan.prototype.configs = configs;
   Morgan.prototype.logger = logger;
+  let timeStart: number;
+  let timeEnd: number;
 
   return (
     request: Drash.Http.Request,
     response?: Drash.Http.Response,
   ) => {
 
-    if (configs && configs.tag_string) {
-      let tagString = configs!.tag_string;
-      if (tagString.includes("{request_method}")) {
-        tagString = tagString.replace("{request_method}", request.method.toUpperCase());
-      }
-      if (tagString.includes("{request_url}")) {
-        tagString = tagString.replace("{request_url}", request.url);
-      }
-      configs.tag_string = tagString;
-    }
-
     // If there is no response, then we know this is occurring before the request
     if (!response) {
+      timeStart = new Date().getTime();
+      // Replace predefined tags with their values
+      if (configs!.tag_string) {
+        let tagString = configs!.tag_string;
+        if (tagString.includes("{request_method}")) {
+          tagString = tagString.replace("{request_method}", request.method.toUpperCase());
+        }
+        if (tagString.includes("{request_url}")) {
+          tagString = tagString.replace("{request_url}", request.url);
+        }
+        configs!.tag_string = tagString;
+      }
       logger.info(`Rquest received.`);
     }
 
     // If there is a response, then we know this is occurring after the request
     if (response) {
-      logger.info(`Response sent.`);
-      logger.error(`Response body: \n${response.body as string}`);
-      logger.debug(`Response `);
+      timeEnd = new Date().getTime();
+      let message = "Response sent.";
+      if (configs!.response_time === true) {
+        message += ` ${getTime(timeEnd, timeStart)}`;
+      }
+      logger.info(message);
     }
   };
+}
+
+function getTime(end: number, start: number): string {
+  return `${end - start} ms`;
 }
