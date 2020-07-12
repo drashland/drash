39a341a03f424c997c4e6175a34bce835a1c5d70
Eric Crooks <eric.crooks@gmail.com>
2020-07-12 05:43:53 -0400

    initial commit with morgan logger

---
+++ morgan_logger/mod.ts
@@ -0,0 +1,54 @@
+import { Drash } from "../deps.ts";
+
+/**
+ * A logger middleware inspired by https://www.npmjs.com/package/morgan.
+ *
+ * @param configs - See Drash.Interfaces.LoggerConfigs.
+ */
+export function MorganLogger(configs?: Drash.Interfaces.LoggerConfigs) {
+  const defaultConfigs = {
+    enabled: true,
+    level: "info",
+    tag_string: "{datetime} | {level} |",
+    tag_string_fns: {
+      datetime() {
+        return new Date().toISOString().replace("T", " ").split(".")[0];
+      }
+    }
+  };
+
+  if (configs) {
+    if (!configs.hasOwnProperty("enabled")) {
+      configs.enabled = defaultConfigs.enabled;
+    }
+    if (!configs.hasOwnProperty("level")) {
+      configs.level = defaultConfigs.level;
+    }
+    if (!configs.hasOwnProperty("tag_string")) {
+      configs.tag_string = defaultConfigs.tag_string;
+    }
+    if (!configs.hasOwnProperty("tag_string_fns")) {
+      configs.tag_string_fns = defaultConfigs.tag_string_fns;
+    }
+  }
+
+  const logger = new Drash.CoreLoggers.ConsoleLogger(
+    configs ?? defaultConfigs
+  );
+
+  return (
+    request: Drash.Http.Request,
+    response?: Drash.Http.Response,
+  ) => {
+    // If there is no response, then we know this is occurring before the request
+    if (!response) {
+      logger.info(`Request received: ${request.method} ${request.url}`);
+    }
+    // If there is a response, then we know this is occurring after the request
+    if (response) {
+      logger.info(
+        `Response: ${response.status_code} ${response.getStatusMessage()}`,
+      );
+    }
+  };
+}
