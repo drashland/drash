c6315c199dbd0ac342134860eb171d01698d07b2
Edward Bebbington <edward.bebbington@intercity.technology>
2020-07-26 00:21:04 +0100

    [update-workflows] Fixed app_3005 integration tests

--- tests/integration/app_3005_middleware_resource_level/middleware_resource_test.ts
+++ tests/integration/app_3005_middleware_resource_level/middleware_resource_test.ts
@@ -1,9 +1,20 @@
 import members from "../../members.ts";
 import { Rhum } from "../../deps.ts";
+import {Drash} from "../../../mod.ts";
+import MiddlewareResource from "./resources/middleware_resource.ts";
+import {runServer} from "../test_utils.ts";
+
+const server = new Drash.Http.Server({
+  response_output: "application/json",
+  resources: [
+    MiddlewareResource,
+  ],
+});
 
 Rhum.testPlan("middleware_resource_test.ts", () => {
   Rhum.testSuite("/middleware", () => {
     Rhum.testCase("resource-level token header is required", async () => {
+      await runServer(server, { port: 3005 })
       let response;
 
       response = await members.fetch.get("http://localhost:3005/middleware", {
@@ -18,6 +29,8 @@ Rhum.testPlan("middleware_resource_test.ts", () => {
 
       response = await members.fetch.get("http://localhost:3005/middleware");
       Rhum.asserts.assertEquals(await response.text(), '"No token, dude."');
+
+      await server.close()
     });
   });
 });
