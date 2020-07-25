2b0901a71e8fffa05479c6d60a65983181d07580
Edward Bebbington <edward.bebbington@intercity.technology>
2020-07-26 00:17:10 +0100

    [update-workflows] Fixed app_3003 integration tests

--- tests/integration/app_3003_middleware/header_token_middleware_resource_test.ts
+++ tests/integration/app_3003_middleware/header_token_middleware_resource_test.ts
@@ -1,9 +1,26 @@
 import members from "../../members.ts";
 import { Rhum } from "../../deps.ts";
+import {Drash} from "../../../mod.ts";
+import HeaderTokenMiddleware from "./middleware/header_token_middleware.ts";
+import HeaderTokenMiddlewareResource from "./resources/header_token_middleware_resource.ts";
+import {runServer} from "../test_utils.ts";
+
+const server = new Drash.Http.Server({
+  response_output: "application/json",
+  middleware: {
+    before_request: [
+      HeaderTokenMiddleware,
+    ],
+  },
+  resources: [
+    HeaderTokenMiddlewareResource,
+  ],
+});
 
 Rhum.testPlan("header_token_middleware_resource_test.ts", () => {
   Rhum.testSuite("/middleware", () => {
     Rhum.testCase("token header is required", async () => {
+      await runServer(server, { port: 3003 })
       let response;
 
       response = await members.fetch.get("http://localhost:3003/middleware", {
@@ -18,6 +35,8 @@ Rhum.testPlan("header_token_middleware_resource_test.ts", () => {
 
       response = await members.fetch.get("http://localhost:3003/middleware");
       Rhum.asserts.assertEquals(await response.text(), '"No token, dude."');
+
+      await server.close()
     });
   });
 });
