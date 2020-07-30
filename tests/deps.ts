791c674156f5d3210bd36d9cafff72d0d8e9d3a1
Eric Crooks <crookse@users.noreply.github.com>
2020-07-29 20:51:26 -0400

    Update deps.ts

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,4 +1,4 @@
-export { Rhum } from "https://deno.land/x/rhum@v1.1.0/mod.ts";
+export { Rhum } from "https://deno.land/x/rhum@v1.x/mod.ts";
 export * as path from "https://deno.land/std@v0.62.0/path/mod.ts";
 export {
   MultipartReader,
