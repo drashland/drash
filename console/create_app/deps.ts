638af2016569e998b5f6ac476390093e970169ae
Eric Crooks <eric.crooks@gmail.com>
2020-07-30 14:01:04 -0400

    [release] bump versions; remove version in package_vue.json

--- console/create_app/deps.ts
+++ console/create_app/deps.ts
@@ -1,2 +1,2 @@
-export { Drash } from "https://deno.land/x/drash@v1.2.0/mod.ts";
+export { Drash } from "https://deno.land/x/drash@v1.2.1/mod.ts";
 export { assertEquals } from "https://deno.land/std@v0.62.0/testing/asserts.ts";
