882f6c6060efe93c142dcca1677576cf97855658
Eric Crooks <eric.crooks@gmail.com>
2020-08-03 20:15:00 -0400

    [release-v1.2.2] bump versions

--- console/create_app/deps.ts
+++ console/create_app/deps.ts
@@ -1,2 +1,2 @@
-export { Drash } from "https://deno.land/x/drash@v1.2.1/mod.ts";
-export { assertEquals } from "https://deno.land/std@v0.62.0/testing/asserts.ts";
+export { Drash } from "https://deno.land/x/drash@v1.2.2/mod.ts";
+export { assertEquals } from "https://deno.land/std@0.63.0/testing/asserts.ts";
