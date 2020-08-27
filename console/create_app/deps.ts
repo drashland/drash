94230dd4e704abfaf4cc6f0c5033572cf1f5318f
Edward Bebbington <edward.bebbington@intercity.technology>
2020-08-27 18:05:55 +0100

    Update workflow deno versions and create app dependencies

--- console/create_app/deps.ts
+++ console/create_app/deps.ts
@@ -1,2 +1,2 @@
 export { Drash } from "https://deno.land/x/drash@v1.2.3/mod.ts";
-export { assertEquals } from "https://deno.land/std@0.65.0/testing/asserts.ts";
+export { assertEquals } from "https://deno.land/std@0.66.0/testing/asserts.ts";
