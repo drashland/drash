0a30cd2a9d9061c5a737cb32f76fe107f52db967
Edward Bebbington <edward.bebbington@intercity.technology>
2020-10-07 04:57:30 +0100

    bump versions

--- mod.ts
+++ mod.ts
@@ -50,7 +50,7 @@ export namespace Drash {
   /**
    * Drash version.
    */
-  export const version: string = "v1.2.4";
+  export const version: string = "v1.2.5";
 
   export namespace Compilers {
     export class TemplateEngine extends BaseTemplateEngine {}
