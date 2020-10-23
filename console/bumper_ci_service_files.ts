89a95e76d9d21efd260fe0394e586d3ccaba967b
Eric Crooks <eric.crooks@gmail.com>
2020-10-22 20:22:00 -0400

    [use-bumper-service] use one script to bump versions

---
+++ console/bumper_ci_service_files.ts
@@ -0,0 +1,48 @@
+export const regexes = {
+  mod: /version = ".+"/g,
+  egg_json: /"version": ".+"/,
+  import_export_statements: /drash@v[0-9\.]+[0-9\.]+[0-9\.]/g,
+  yml_deno: /deno: \[".+"\]/g,
+};
+
+export const moduleVersionFiles = [
+  {
+    filename: "./egg.json",
+    replaceTheRegex: regexes.egg_json,
+    replaceWith: `"version": "{{ thisModulesLatestVersion }}"`,
+  },
+  {
+    filename: "./README.md",
+    replaceTheRegex: regexes.import_export_statements,
+    replaceWith: `drash@v{{ thisModulesLatestVersion }}`,
+  },
+  {
+    filename: "./mod.ts",
+    replaceTheRegex: regexes.mod,
+    replaceWith: `version = "{{ thisModulesLatestVersion }}"`,
+  },
+  {
+    filename: "./create_app/deps.ts",
+    replaceTheRegex: regexes.import_export_statements,
+    replaceWith: `drash@v{{ thisModulesLatestVersion }}`,
+  }
+];
+
+export const denoVersionFiles = [
+  {
+    filename: "./.github/workflows/master.yml",
+    replaceTheRegex: regexes.yml_deno,
+    replaceWith: `deno: ["{{ latestDenoVersion }}"]`,
+  },
+  {
+    filename: "./.github/workflows/bumper.yml",
+    replaceTheRegex: regexes.yml_deno,
+    replaceWith: `deno: ["{{ latestDenoVersion }}"]`,
+  },
+  {
+    filename: "./.github/workflows/pre_release.yml",
+    replaceTheRegex: regexes.yml_deno,
+    replaceWith: `deno: ["{{ latestDenoVersion }}"]`,
+  },
+];
+
