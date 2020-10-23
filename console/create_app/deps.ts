93cbf6a35436db0ff94473768e1a185617ac7a81
Eric Crooks <eric.crooks@gmail.com>
2020-10-22 20:26:52 -0400

    [use-bumper-service] update deps in create_app script

--- console/create_app/deps.ts
+++ console/create_app/deps.ts
@@ -1,2 +1,2 @@
-export { Drash } from "https://deno.land/x/drash@v1.2.3/mod.ts";
-export { assertEquals } from "https://deno.land/std@0.71.0/testing/asserts.ts";
+export { Drash } from "https://deno.land/x/drash@v1.2.5/mod.ts";
+export { assertEquals } from "https://deno.land/std@0.74.0/testing/asserts.ts";
