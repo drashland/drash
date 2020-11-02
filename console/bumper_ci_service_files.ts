9ae46084b9069d0e302726294addb2016e2f8734
Edward Bebbington <edward.bebbington@intercity.technology>
2020-11-02 18:54:08 +0000

    [complete-ci] Fix bumper ci on pre release

--- console/bumper_ci_service_files.ts
+++ console/bumper_ci_service_files.ts
@@ -8,7 +8,8 @@ export const regexes = {
   drash_import_statements: /drash@v[0-9\.]+[0-9\.]+[0-9\.]/g
 };
 
-const latestDrashVersion = new BumperService().getModulesLatestVersion("drash");
+const bumperService = new BumperService("drash_middleware")
+const latestDrashVersion = await bumperService.getModulesLatestVersion("drash");
 
 export const bumpVersionFiles = [
   {
