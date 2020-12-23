db8227d7d476eb0139457b3c8f3c3aec3e78e23e
crookse <crookse@users.noreply.github.com>
2020-12-23 00:43:58 +0000

    Update dependencies and bump version numbers across repo

--- csrf/deps.ts
+++ csrf/deps.ts
@@ -1,2 +1,2 @@
-export { createHash } from "https://deno.land/std@0.81.0/hash/mod.ts";
-export { v4 } from "https://deno.land/std@0.81.0/uuid/mod.ts";
+export { createHash } from "https://deno.land/std@0.82.0/hash/mod.ts";
+export { v4 } from "https://deno.land/std@0.82.0/uuid/mod.ts";
