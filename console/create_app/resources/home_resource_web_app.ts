3d9199b6f19e0ba98f869bcf91135a35821e8634
Eric Crooks <eric.crooks@gmail.com>
2020-12-26 04:34:20 -0500

    [#430-remove-template-engine] update create app to use read the index.html file

--- console/create_app/resources/home_resource_web_app.ts
+++ console/create_app/resources/home_resource_web_app.ts
@@ -1,10 +1,13 @@
 import { Drash } from "../deps.ts";
+const decoder = new TextDecoder();
 
 export default class HomeResource extends Drash.Http.Resource {
   static paths = ["/"];
 
-  public GET() {
-    this.response.body = this.response.render("/index.html");
+  public async GET() {
+    this.response.body = decoder.decode(
+      await Deno.readFile("./public/index.html");
+    );
     return this.response;
   }
 
