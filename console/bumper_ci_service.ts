01d1bc77d56158b3ecc11af097cb543db6f87e72
Edward Bebbington <47337480+ebebbington@users.noreply.github.com>
2020-12-31 13:09:40 +0000

    Update bumper_ci_service.ts

--- console/bumper_ci_service.ts
+++ console/bumper_ci_service.ts
@@ -1,10 +1,10 @@
 import { BumperService } from "https://raw.githubusercontent.com/drashland/services/v0.0.1/ci/bumper_service.ts";
-import { bumperFiles, preReleaseFiles } from "./bumper_ci_service_files.ts";
+import { preReleaseFiles } from "./bumper_ci_service_files.ts";
 
 const b = new BumperService("drash", Deno.args);
 
 if (b.isForPreRelease()) {
   b.bump(preReleaseFiles);
 } else {
-  b.bump(bumperFiles);
+  //b.bump(bumperFiles);
 }
