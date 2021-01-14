4f5a6a87f07b8d79e9b6755bdcd12d9cdef75839
crookse <crookse@users.noreply.github.com>
2021-01-14 01:14:41 +0000

    Update dependencies and bump version numbers across repo

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,8 +1,8 @@
-export { Rhum } from "https://deno.land/x/rhum@v1.1.5/mod.ts";
+export { Rhum } from "https://deno.land/x/rhum@v1.1.6/mod.ts";
 
-export * as path from "https://deno.land/std@0.80.0/path/mod.ts";
+export * as path from "https://deno.land/std@0.83.0/path/mod.ts";
 
 export {
   isFormFile,
   MultipartReader,
-} from "https://deno.land/std@0.80.0/mime/multipart.ts";
+} from "https://deno.land/std@0.83.0/mime/multipart.ts";
