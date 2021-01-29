843e600bdd5a3b335d04b4252019494870aaa3e1
drashbot <drashbot@users.noreply.github.com>
2021-01-29 23:02:58 +0000

    Make necessary pre-release changes

--- console/create_app/deps.ts
+++ console/create_app/deps.ts
@@ -1,2 +1,2 @@
-export { Drash } from "https://deno.land/x/drash@v1.4.0/mod.ts";
+export { Drash } from "https://deno.land/x/drash@v1.4.1/mod.ts";
 export { assertEquals } from "https://deno.land/std@0.84.0/testing/asserts.ts";
