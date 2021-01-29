f9909e9ab455fb44195c937e9e112788fe069283
drashbot <59404681+drashbot@users.noreply.github.com>
2021-01-29 18:09:45 -0500

    Make necessary pre-release changes (#472)
    Co-authored-by: drashbot <drashbot@users.noreply.github.com>

--- mod.ts
+++ mod.ts
@@ -49,7 +49,7 @@ export namespace Drash {
   /**
    * Drash version.
    */
-  export const version = "v1.3.2";
+  export const version = "v1.4.1";
 
   export namespace Dictionaries {
     export const LogLevels = log_levels.LogLevels;
