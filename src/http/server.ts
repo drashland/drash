46b960a57627a77ef8f9bd8bd99d9dcc9de188df
Eric Crooks <eric.crooks@gmail.com>
2021-02-11 11:01:29 -0500

    [#update-dependencies-bump-version-numbers] deno fmt

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,10 @@
 import { Drash } from "../../mod.ts";
 import {
+  ConsoleLogger,
   HTTPOptions,
   HTTPSOptions,
   IndexService,
   ISearchResult,
-  ConsoleLogger,
   serve,
   Server as DenoServer,
   ServerRequest,
