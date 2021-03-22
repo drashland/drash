97ac62822234062d7697da90cd2f8af7b1e655aa
drashbot <drashbot@users.noreply.github.com>
2021-03-22 20:33:38 +0000

    Make necessary pre-release changes

--- console/create_app/deps.ts
+++ console/create_app/deps.ts
@@ -1,2 +1,2 @@
-export { Drash } from "https://deno.land/x/drash@v1.4.1/mod.ts";
+export { Drash } from "https://deno.land/x/drash@v1.4.2/mod.ts";
 export { assertEquals } from "https://deno.land/std@0.91.0/testing/asserts.ts";
--- mod.ts
+++ mod.ts
@@ -49,7 +49,7 @@ export namespace Drash {
   /**
    * Drash version.
    */
-  export const version = "v1.4.1";
+  export const version = "v1.4.2";
 
   export namespace Dictionaries {
     export const LogLevels = log_levels.LogLevels;
