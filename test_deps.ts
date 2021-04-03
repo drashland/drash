14c97465ea57ba7a02386a2e939c0a221b34c531
Eric Crooks <eric.crooks@gmail.com>
2021-04-03 11:06:10 -0400

    chore: bump versions

--- test_deps.ts
+++ test_deps.ts
@@ -1,6 +1,6 @@
-import { ServerRequest } from "https://deno.land/std@0.82.0/http/server.ts";
+import { ServerRequest } from "https://deno.land/std@0.92.0/http/server.ts";
 
-export { Rhum } from "https://deno.land/x/rhum@v1.1.4/mod.ts";
+export { Rhum } from "https://deno.land/x/rhum@v1.1.9/mod.ts";
 
 // deno-lint-ignore no-explicit-any
 export const mockRequest = (url = "/", method = "get"): any => {
