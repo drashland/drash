eb3af30d762338afc4f0339e231ec687ad5598ae
crookse <crookse@users.noreply.github.com>
2021-06-13 00:46:21 +0000

    Update dependencies and bump version numbers across repo

--- console/create_app/deps.ts
+++ console/create_app/deps.ts
@@ -1,2 +1,2 @@
-export { Drash } from "https://deno.land/x/drash@v1.5.0/mod.ts";
+export { Drash } from "https://deno.land/x/drash@v1.4.4/mod.ts";
 export { assertEquals } from "https://deno.land/std@0.98.0/testing/asserts.ts";
