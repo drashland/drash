9b5cf082f572b6a51c937ca3c5acd3c791bfc314
Eric Crooks <eric.crooks@gmail.com>
2021-06-27 16:50:23 -0400

    refactor: put middleware in directories

--- src/services/csrf/deps.ts
+++ src/services/csrf/deps.ts
@@ -1,3 +1,2 @@
 export { createHash } from "https://deno.land/std@0.98.0/hash/mod.ts";
 export { v4 } from "https://deno.land/std@0.98.0/uuid/mod.ts";
-export { vary } from "https://raw.githubusercontent.com/dmpjs/vary/v1.1.0/mod.ts";
