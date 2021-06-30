e498349ebeab24ee1d594324326fe2744350cdf7
crookse <crookse@users.noreply.github.com>
2021-06-30 00:47:57 +0000

    Update dependencies and bump version numbers across repo

--- console/create_app/deps.ts
+++ console/create_app/deps.ts
@@ -1,2 +1,2 @@
 export { Drash } from "https://deno.land/x/drash@v1.5.0/mod.ts";
-export { assertEquals } from "https://deno.land/std@0.99.0/testing/asserts.ts";
+export { assertEquals } from "https://deno.land/std@0.100.0/testing/asserts.ts";
--- tests/deps.ts
+++ tests/deps.ts
@@ -1,8 +1,8 @@
 export { Rhum } from "https://deno.land/x/rhum@v1.1.10/mod.ts";
 
-export * as path from "https://deno.land/std@0.99.0/path/mod.ts";
+export * as path from "https://deno.land/std@0.100.0/path/mod.ts";
 
 export {
   isFormFile,
   MultipartReader,
-} from "https://deno.land/std@0.99.0/mime/multipart.ts";
+} from "https://deno.land/std@0.100.0/mime/multipart.ts";
