24fac5c359e3255c066122541245c95abb154818
Eric Crooks <eric.crooks@gmail.com>
2021-07-17 00:09:24 -0400

    feat: put back response class

---
+++ src/http/response.ts
@@ -0,0 +1,41 @@
+import {
+  Cookie,
+  deleteCookie,
+  encoder,
+  setCookie,
+  Status,
+  STATUS_TEXT,
+} from "../../deps.ts";
+import { ICreateable, IResponseOptions, IResponseOutput } from "../interfaces.ts";
+import { Request } from "./request.ts";
+
+export interface IOptions {
+  default_content_type?: string;
+}
+
+/**
+ * Response handles sending a response to the client making the request.
+ */
+export class Response implements ICreateable {
+
+  public body: unknown = undefined;
+
+  public headers = new Headers();
+
+  protected options: IResponseOptions = {};
+
+  //////////////////////////////////////////////////////////////////////////////
+  // FILE MARKER - METHODS - FACTORY ///////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+
+  public create(): void {
+    this.headers.set(
+      "Content-Type",
+      this.options.default_response_content_type!
+    );
+  }
+
+  public addOptions(options: IResponseOptions): void {
+    this.options = options;
+  }
+}
