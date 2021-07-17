f8d2194ff9a0afcc2ac3fd347ba4dbc6446f1266
Eric Crooks <eric.crooks@gmail.com>
2021-07-17 00:06:48 -0400

    BREAKING!: remove create_app script (to be put somewhere else)

--- console/create_app/app_web_app.ts
+++
@@ -1,8 +0,0 @@
-import { server } from "./server.ts";
-
-await server.run({
-  hostname: "localhost",
-  port: 1667,
-});
-
-console.log(`Server listening: http://${server.hostname}:${server.port}`);
--- console/create_app/deps.ts
+++
@@ -1,2 +0,0 @@
-export { Drash } from "https://deno.land/x/drash@v1.5.0/mod.ts";
-export { assertEquals } from "https://deno.land/std@0.99.0/testing/asserts.ts";
