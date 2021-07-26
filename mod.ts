cdde7f07a6ba02b37843dfdf7b27a0c51318b454
Eric Crooks <eric.crooks@gmail.com>
2021-07-25 20:44:16 -0400

    chore: clean up mod

--- mod.ts
+++ mod.ts
@@ -24,13 +24,28 @@ import { Service } from "./src/http/service.ts";
 
 export const version = "v1.5.0";
 
+// Dictionaries
+export { mimeDb as MimeDb } from "./src/dictionaries/mime_db.ts";
+
+// Deps
 export * as Deps from "./deps.ts";
+
+// Errors
 export * as Errors from "./src/errors.ts";
+
+// Gurus
 export { Prototype } from "./src/gurus/prototype.ts";
+
+// Handlers
+export { MiddlewareHandler } from "./src/handlers/middleware_handler.ts";
+export { ResourceHandler } from "./src/handlers/resource_handler.ts";
+
+// Http
 export { Request } from "./src/http/request.ts";
 export { Resource } from "./src/http/resource.ts";
 export { Response } from "./src/http/response.ts";
-export { mimeDb as MimeDb } from "./src/dictionaries/mime_db.ts";
+
+// Export members from the IMPORTS section above
 export {
   Factory,
   Interfaces,
@@ -39,6 +54,9 @@ export {
   Types,
 };
 
+/**
+ * Create a Drash HTTP/HTTPS server.
+ */
 export function createServer(options: Interfaces.IServerOptions = {}): Server {
   return Factory.create(Server, options);
 }
