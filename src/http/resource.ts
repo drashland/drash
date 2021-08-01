0d0a8c2f5d30f8a414ed1f957c33f04557a221ea
Eric Crooks <eric.crooks@gmail.com>
2021-08-01 00:11:12 -0400

    chore: clean up the resource object

--- src/http/resource.ts
+++ src/http/resource.ts
@@ -9,73 +9,46 @@ import * as Drash from "../../mod.ts";
  *     https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Identifying_resources_on_the_Web
  */
 export class Resource implements Drash.Interfaces.IResource {
-  /**
-     * A property to hold the middleware this resource uses.
-     */
   public middleware: { after_request?: []; before_request?: [] } = {};
-
-  /**
-   * The path params (if any), which are taken from this resource's URIs.
-   */
-  public path_params!: string;
-
-  /**
-   * The URI paths that this resource is located at.
-   */
+  public path_parameters!: string;
   public uri_paths: string[] = [];
-
-  /**
-   * A property to hold the expanded version of this object's URIs. An example
-   * of the expanded version is as follows:
-   *
-   *     {
-   *       og_path: "/:id",
-   *       regex_path: "^([^/]+)/?$",
-   *       params: ["id"],
-   *     }
-   */
   public uri_paths_parsed: Drash.Interfaces.IResourcePathsParsed[] = [];
-
-  /**
-   * The request object.
-   */
-  // @ts-ignore: See mod.ts TS IGNORE NOTES > NOTE 1.
-  protected request: Drash.Request;
-
-  /**
-   * The server object.
-   */
-  // @ts-ignore: See mod.ts TS IGNORE NOTES > NOTE 1.
-  protected server: Drash.Server;
-
-  /**
-   * The response object.
-   */
-  // @ts-ignore: See mod.ts TS IGNORE NOTES > NOTE 1.
-  public response: Drash.Response;
-
-  /**
-   * This object's options.
-   */
-  // @ts-ignore: See mod.ts TS IGNORE NOTES > NOTE 1.
-  protected options: Drash.Interfaces.IResourceOptions = {};
+  public response!: Drash.Response;
+  public request!: Drash.Request;
+  public server!: Drash.Server;
+  #options: Drash.Interfaces.IResourceOptions = {};
 
   //////////////////////////////////////////////////////////////////////////////
   // FILE MARKER - METHODS - FACTORY ///////////////////////////////////////////
   //////////////////////////////////////////////////////////////////////////////
 
   /**
-   * See ICreateable#create.
+   * See Drash.Interfaces.ICreateable.create().
    */
   public create(options: Drash.Interfaces.IResourceOptions): void {
-    this.options = options;
-
-    this.server = this.options.server!;
+    this.#setOptions(options);
+    this.#setProperties();
 
     const contentType = this.server.options.default_response_content_type!;
 
     this.response = Drash.Factory.create(Drash.Response, {
       default_response_content_type: contentType,
     });
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+  // FILE MARKER - METHODS - PRIVATE ///////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+
+  #setOptions(options: Drash.Interfaces.IResourceOptions): void {
+    if (!options.server) {
+      throw new Drash.Errors.DrashError("D1007");
+    }
+
+    this.#options = options;
+  }
+
+  #setProperties(): void {
+    this.server = this.#options.server!;
   }
 }
