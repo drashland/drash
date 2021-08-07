b45015cfcbb92959b8d2df500b61c9c56e2a9aa5
drashbot <59404681+drashbot@users.noreply.github.com>
2021-08-07 06:39:33 -0400

    Update dependencies and bump version numbers across repo (#528)
    Co-authored-by: crookse <crookse@users.noreply.github.com>

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,4 +1,4 @@
-export { Rhum } from "https://deno.land/x/rhum@v1.1.10/mod.ts";
+export { Rhum } from "https://deno.land/x/rhum@v1.1.11/mod.ts";
 
 export * as path from "https://deno.land/std@0.103.0/path/mod.ts";
 
