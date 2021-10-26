f6459dac85ef28c44e8def1cd7bf2c4529375811
Edward Bebbington <EdwardSBebbington@hotmail.com>
2021-10-26 20:23:34 +0100

    chore: Add tls tests

---
+++ tests/integration/https_test.ts
@@ -0,0 +1,62 @@
+/**
+ * This test addresses an issue where someone on the discord had their default
+ * content type set, but on  browser requests the response  was "null". This is
+ * because originally, the response class didn't fully take into account the
+ * config AND the accept headers.  Essentially meaning, returning text/html (as
+ * this was the first type on the request)
+ */
+
+import { Rhum, TestHelpers } from "../deps.ts";
+import { Request, Resource, Response, Server } from "../../mod.ts";
+
+////////////////////////////////////////////////////////////////////////////////
+// FILE MARKER - APP SETUP /////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+class BrowserRequestResource extends Resource {
+  paths = ["/browser-request"];
+
+  public GET(_request: Request, response: Response) {
+    response.text("hello");
+  }
+}
+
+const server = new Server({
+  resources: [
+    BrowserRequestResource,
+  ],
+  protocol: "https",
+  hostname: "localhost",
+  port: 3000,
+  key_file: "./tests/data/server.key",
+  cert_file: "./tests/data/server.crt",
+});
+
+////////////////////////////////////////////////////////////////////////////////
+// FILE MARKER - TESTS /////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+Rhum.testPlan("browser_request_resource.ts", () => {
+  Rhum.testSuite("GET /browser-request", () => {
+    Rhum.testCase("Response should be JSON", async () => {
+      server.run();
+      // Example browser request
+      const response = await TestHelpers.makeRequest.get(
+        "https://localhost:3000/browser-request",
+        {
+          headers: {
+            Accept: "*/*",
+          },
+        },
+      );
+      await server.close();
+      Rhum.asserts.assertEquals(await response.text(), "hello");
+      Rhum.asserts.assertEquals(
+        response.headers.get("Content-Type"),
+        "text/plain",
+      );
+    });
+  });
+});
+
+Rhum.run();
