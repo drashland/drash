35802564400ba762d242a6ca34967144869106ab
Edward Bebbington <47337480+ebebbington@users.noreply.github.com>
2021-10-27 23:22:55 +0100

    Update tengine.ts

--- src/services/tengine/tengine.ts
+++ src/services/tengine/tengine.ts
@@ -2,10 +2,8 @@ import { IService, Request, Response, Service } from "../../../mod.ts";
 import { Jae } from "./jae.ts";
 
 interface IOptions {
-  render:
-    ((...args: unknown[]) => Promise<boolean | string> | boolean | string);
   // deno-lint-ignore camelcase
-  views_path?: string;
+  views_path: string;
 }
 
 export class TengineService extends Service implements IService {
@@ -18,20 +16,18 @@ export class TengineService extends Service implements IService {
     this.#options = options;
   }
 
-  runAfterResource(_request: Request, response: Response) {
+  runBeforeResource(_request: Request, response: Response) {
     response.headers.set("Content-Type", "text/html");
 
-    if (this.#options.views_path) {
-      if (!this.#templateEngine) {
-        this.#templateEngine = new Jae(this.#options.views_path);
-      }
-      this.#options.render = (...args: unknown[]): string => {
-        return this.#templateEngine!.render(
-          args[0] as string,
-          args[1] as unknown,
-        );
-      };
+    if (!this.#templateEngine) {
+      this.#templateEngine = new Jae(this.#options.views_path);
     }
+    this.#options.render = (...args: unknown[]): string => {
+      return this.#templateEngine!.render(
+        args[0] as string,
+        args[1] as unknown,
+      );
+    };
 
     if (response.render) {
       response.render = this.#options.render;
