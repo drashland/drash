8673716d263629c9ecf17069122a519abb20f2da
Edward Bebbington <EdwardSBebbington@hotmail.com>
2021-11-07 21:06:42 +0000

    feat: Add response time service

---
+++ src/services/response_time/response_time.ts
@@ -0,0 +1,21 @@
+import { IService, Request, Response, Service } from "../../../mod.ts";
+
+export class ResponseTimeService extends Service implements IService {
+  #startTime = 0;
+
+  #endTime = 0;
+
+  constructor() {
+    super();
+  }
+
+  runBeforeResource() {
+    this.#startTime = new Date().getTime();
+  }
+
+  runAfterResource(_request: Request, response: Response) {
+    this.#endTime = new Date().getTime();
+    const time = (this.#endTime - this.#startTime) + "ms";
+    response.headers.set("X-Response-Time", time.toString());
+  }
+}
