4e66f5ded738a6cac663a85fde0d9c9703917eee
Edward Bebbington <EdwardSBebbington@hotmail.com>
2021-11-07 16:13:34 +0000

    re add render method; add test

--- src/services/tengine/tengine.ts
+++ src/services/tengine/tengine.ts
@@ -2,8 +2,10 @@ import { IService, Request, Response, Service } from "../../../mod.ts";
 import { Jae } from "./jae.ts";
 
 interface IOptions {
+  render:
+    ((...args: unknown[]) => Promise<boolean | string> | boolean | string);
   // deno-lint-ignore camelcase
-  views_path: string;
+  views_path?: string;
 }
 
 export class TengineService extends Service implements IService {
@@ -19,15 +21,17 @@ export class TengineService extends Service implements IService {
   runBeforeResource(_request: Request, response: Response) {
     response.headers.set("Content-Type", "text/html");
 
-    if (!this.#templateEngine) {
-      this.#templateEngine = new Jae(this.#options.views_path);
+    if (this.#options.views_path) {
+      if (!this.#templateEngine) {
+        this.#templateEngine = new Jae(this.#options.views_path);
+      }
+      this.#options.render = (...args: unknown[]): string => {
+        return this.#templateEngine!.render(
+          args[0] as string,
+          args[1] as unknown,
+        );
+      };
     }
-    this.#options.render = (...args: unknown[]): string => {
-      return this.#templateEngine!.render(
-        args[0] as string,
-        args[1] as unknown,
-      );
-    };
 
     if (response.render) {
       response.render = this.#options.render;
---
+++ tests/integration/tengine_test.ts
@@ -0,0 +1,62 @@
+/**
+ * This test addresses an issue where someone on the discord had their default
+ * content type set, but on  browser requests the response  was "null". This is
+ * because originally, the response class didn't fully take into account the
+ * config AND the accept headers.  Essentially meaning, returning text/html (as
+ * this was the first type on the request)
+ */
+
+ import { Rhum } from "../deps.ts";
+ import { Request, Resource, Response, Server } from "../../mod.ts";
+ import { TengineService } from "../../src/services/tengine/tengine.ts"
+
+ const tengine = new TengineService({
+     views_path: "./tests/data/views",
+     render: (..._args: unknown[]) => false
+ })
+ 
+ ////////////////////////////////////////////////////////////////////////////////
+ // FILE MARKER - APP SETUP /////////////////////////////////////////////////////
+ ////////////////////////////////////////////////////////////////////////////////
+ 
+ class TengineResource extends Resource {
+   paths = ["/tengine"];
+
+   services = {
+       'GET': [tengine]
+   }
+ 
+   public GET(_request: Request, response: Response) {
+        response.html(response.render("/tengine_index.html", {
+         greeting: "Gday"
+     }) as string);
+   }
+ }
+ 
+ const server = new Server({
+   resources: [
+     TengineResource,
+   ],
+   protocol: "http",
+   hostname: "localhost",
+   port: 3000,
+ });
+ 
+ ////////////////////////////////////////////////////////////////////////////////
+ // FILE MARKER - TESTS /////////////////////////////////////////////////////////
+ ////////////////////////////////////////////////////////////////////////////////
+ 
+ Rhum.testPlan("tengine_test.ts", () => {
+   Rhum.testSuite("GET /tengine", () => {
+     Rhum.testCase("Tengine should handle the request", async () => {
+       server.run();
+       const res = await fetch(`${server.address}/tengine`);
+       await server.close();
+       Rhum.asserts.assertEquals(res.headers.get('content-type'), 'text/html')
+       Rhum.asserts.assertEquals(await res.text(), "Gday");
+     });
+   });
+ });
+ 
+ Rhum.run();
+ 
