0e33ba01987f3317dcfdc7a9c7af145fbab99251
Edward Bebbington <EdwardSBebbington@hotmail.com>
2021-11-08 20:04:15 +0000

    cleanup

--- src/services/etag/etag.ts
+++ src/services/etag/etag.ts
@@ -1,13 +1,6 @@
 import { IService, Request, Response, Service } from "../../../mod.ts";
 import { createHash } from "./deps.ts";
 
-interface IOptions {
-  render:
-    ((...args: unknown[]) => Promise<boolean | string> | boolean | string);
-  // deno-lint-ignore camelcase
-  views_path?: string;
-}
-
 export class EtagService extends Service implements IService {
   runAfterResource(_request: Request, response: Response) {
     if (
@@ -20,10 +13,6 @@ export class EtagService extends Service implements IService {
     const body = typeof response.body === "string"
       ? new TextEncoder().encode(response.body)
       : response.body as Uint8Array;
-    // const buffer = createHash('sha1')
-    //   .update(body)
-    //   .digest()
-    // const hash = new TextDecoder().decode(buffer).substring(0, 27)
     const hash = createHash("sha1").update(body, "utf8").digest("base64")
       .toString().substring(0, 27);
     const len = body.byteLength;
