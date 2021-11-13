67038ebf0423e048d305ba843c30e4d763c1179b
Eric Crooks <eric.crooks@gmail.com>
2021-11-12 21:56:08 -0500

    refactor(graphql): export all graphql members so it can be used by devs

--- src/services/graphql/deps.ts
+++ src/services/graphql/deps.ts
@@ -1,7 +1,4 @@
-export {
-  graphql,
-  GraphQLSchema,
-} from "https://cdn.skypack.dev/graphql@15.5.0?dts"; // TODO ENSURE DMM UPDATES THIS
+export * as GraphQL from "https://cdn.skypack.dev/graphql@15.5.0?dts"; // TODO ENSURE DMM UPDATES THIS
 export { renderPlaygroundPage } from "https://deno.land/x/gql@0.1.2/graphiql/render.ts";
 export type {
   ExecutionResult,
