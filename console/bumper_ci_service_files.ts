809b91c11dcd94040a7432bc6e863387f2f6d1bb
Edward Bebbington <47337480+ebebbington@users.noreply.github.com>
2021-11-16 00:49:16 +0000

    chore: More maintainable way to produce the version (#579)

--- console/bumper_ci_service_files.ts
+++ console/bumper_ci_service_files.ts
@@ -21,14 +21,4 @@ export const preReleaseFiles: IFile[] = [
     replaceTheRegex: regexes.egg_json,
     replaceWith: `"version": "{{ thisModulesLatestVersion }}"`,
   },
-  {
-    filename: "./mod.ts",
-    replaceTheRegex: regexes.const_statements,
-    replaceWith: `version = "v{{ thisModulesLatestVersion }}"`,
-  },
-  {
-    filename: "./console/create_app/deps.ts",
-    replaceTheRegex: regexes.import_export_statements,
-    replaceWith: `drash@v{{ thisModulesLatestVersion }}`,
-  },
 ];
