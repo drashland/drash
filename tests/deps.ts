d818dd5c1d033c3dbfbd44ef7bf805947d22f95a
Eric Crooks <eric.crooks@gmail.com>
2022-01-09 19:05:29 -0500

    chore(deps): bump to std@0.120.0

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,12 +1,12 @@
 export { Rhum } from "https://deno.land/x/rhum@v1.1.12/mod.ts";
 export * as Drash from "../mod.ts";
-export * as path from "https://deno.land/std@0.119.0/path/mod.ts";
+export * as path from "https://deno.land/std@0.120.0/path/mod.ts";
 export * as TestHelpers from "./test_helpers.ts";
 export {
   assertEquals,
   assertNotEquals,
-} from "https://deno.land/std@0.119.0/testing/asserts.ts";
-export { BufReader } from "https://deno.land/std@0.119.0/io/bufio.ts";
+} from "https://deno.land/std@0.120.0/testing/asserts.ts";
+export { BufReader } from "https://deno.land/std@0.120.0/io/bufio.ts";
 
-export { green, red } from "https://deno.land/std@0.119.0/fmt/colors.ts";
-export { delay } from "https://deno.land/std@0.119.0/async/delay.ts";
+export { green, red } from "https://deno.land/std@0.120.0/fmt/colors.ts";
+export { delay } from "https://deno.land/std@0.120.0/async/delay.ts";
