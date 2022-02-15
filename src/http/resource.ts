b10a3ecb886c5ef5aa1120fb27483f114b663051
Eric Crooks <eric.crooks@gmail.com>
2022-02-14 22:40:49 -0500

    fix(http/resource): remove hardcoded status and allow status and headers to be specified for .redirect()
    Trying to make this more like the response class' body methods.

--- src/http/resource.ts
+++ src/http/resource.ts
@@ -12,20 +12,34 @@ export class Resource implements Drash.Interfaces.IResource {
   public services: Drash.Interfaces.IResourceServices = {};
   public paths: string[] = [];
 
+  // TODO(crookse) Deprecate this method and introduce `response.redirect()` so
+  // it can leverage `this.#setHeaders()`.
   /**
    * Redirect the incoming request to another resource
    *
    * @example
    * ```js
    * this.redirect("http://localhost/login", response);
-   * return;
+   * this.redirect("http://localhost/login", response, 301);
    * ```
    *
-   * @param location - The location or resource uri of where you want to redirect the request to
-   * @param response - The response object, to set the related headers and status code on
+   * @param location - The location or resource uri of where you want to
+   * redirect the request to
+   * @param response - The response object, to set the related headers and
+   * status code on
+   * @param status - (optional) The response status. Defaults to 302.
+   * @param headers - (optional) Any extra headers to specify with the response.
    */
-  public redirect(location: string, response: Drash.Response): void {
+  public redirect(
+    location: string,
+    response: Drash.Response,
+    status: Drash.Types.HttpStatusRedirection = 302,
+    headers: Drash.Types.HttpHeadersKeyValuePairs = {},
+  ): void {
     response.headers.set("Location", location);
-    response.status = 302;
+    response.status = status;
+    Object.keys(headers).forEach((key) => {
+      response.headers.set(key, headers[key]);
+    });
   }
 }
