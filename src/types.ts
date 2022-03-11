5458538fed77529543576a900bc22b9502662bd3
Eric Crooks <eric.crooks@gmail.com>
2022-03-11 08:20:24 -0500

    chore(types): add reusable types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import { Request, Resource, Response } from "../mod.ts";
+
 /**
  * The allowed types for an HTTP method on a resource.
  */
@@ -20,3 +22,13 @@ export type BodyFile = {
 };
 
 export type HttpHeadersKeyValuePairs = Record<string, string>;
+
+export type ResourcesAndPatternsMap = Map<number, {
+  resource: Resource;
+  patterns: URLPattern[];
+}>;
+
+export type ResourceHttpMethodHandler = (
+  request: Request,
+  response: Response,
+) => Promise<void> | void;
