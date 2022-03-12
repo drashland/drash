4c56543d1105e4c818166b5a4e7f47d26a2a3c64
Eric Crooks <eric.crooks@gmail.com>
2022-03-11 22:17:46 -0500

    chore: post-rebase fix

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,4 +1,4 @@
-export { Rhum } from "https://deno.land/x/rhum@v1.1.13/mod.ts";
+import { Rhum } from "https://deno.land/x/rhum@v1.1.13/mod.ts";
 export * as Drash from "../mod.ts";
 export * as path from "https://deno.land/std@0.128.0/path/mod.ts";
 export * as TestHelpers from "./test_helpers.ts";
