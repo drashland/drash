1b401fb0f8eaba3564ed24d6d6a8a9d9fad681dc
Eric Crooks <eric.crooks@gmail.com>
2022-05-28 22:05:25 -0400

    chore: update services' deps

--- src/services/csrf/deps.ts
+++ src/services/csrf/deps.ts
@@ -1,2 +1,2 @@
-export { createHash } from "https://deno.land/std@0.120.0/hash/mod.ts";
-export { v4 } from "https://deno.land/std@0.120.0/uuid/mod.ts";
+export { createHash } from "https://deno.land/std@0.141.0/hash/mod.ts";
+export { v4 } from "https://deno.land/std@0.141.0/uuid/mod.ts";
--- src/services/graphql/deps.ts
+++ src/services/graphql/deps.ts
@@ -1,6 +1,6 @@
 export * as GraphQL from "https://cdn.skypack.dev/graphql@15.5.0?dts";
 // TODO ENSURE DMM UPDATES THIS
-export { renderPlaygroundPage } from "https://deno.land/x/gql@0.1.2/graphiql/render.ts";
+export { renderPlaygroundPage } from "https://deno.land/x/gql@1.1.1/graphiql/render.ts";
 export type {
   ExecutionResult,
 } from "https://cdn.skypack.dev/graphql@15.5.0?dts"; // TODO ENSURE DMM UPDATES THIS
