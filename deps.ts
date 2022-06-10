53a8bc762176b6f5008f015ef38947767c910030
drashbot <59404681+drashbot@users.noreply.github.com>
2022-06-10 00:41:33 +0000

    chore: update deps

--- deps.ts
+++ deps.ts
@@ -2,17 +2,17 @@ const decoder = new TextDecoder();
 const encoder = new TextEncoder();
 export { decoder, encoder };
 
-export { STATUS_TEXT } from "https://deno.land/std@0.141.0/http/http_status.ts";
+export { STATUS_TEXT } from "https://deno.land/std@0.143.0/http/http_status.ts";
 
 export {
   deleteCookie,
   getCookies,
   setCookie,
-} from "https://deno.land/std@0.141.0/http/cookie.ts";
+} from "https://deno.land/std@0.143.0/http/cookie.ts";
 
-export type { Cookie } from "https://deno.land/std@0.141.0/http/cookie.ts";
+export type { Cookie } from "https://deno.land/std@0.143.0/http/cookie.ts";
 
 export {
   Server as StdServer,
-} from "https://deno.land/std@0.141.0/http/server.ts";
-export type { ConnInfo } from "https://deno.land/std@0.141.0/http/server.ts";
+} from "https://deno.land/std@0.143.0/http/server.ts";
+export type { ConnInfo } from "https://deno.land/std@0.143.0/http/server.ts";
