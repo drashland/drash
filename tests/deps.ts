bf4b3afd2a983328b8fba582ceffdbb511837dea
drashbot <59404681+drashbot@users.noreply.github.com>
2022-07-01 00:45:36 +0000

    chore: update deps

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,11 +1,11 @@
 export * as Drash from "../mod.ts";
-export * as path from "https://deno.land/std@0.145.0/path/mod.ts";
+export * as path from "https://deno.land/std@0.146.0/path/mod.ts";
 export * as TestHelpers from "./test_helpers.ts";
 export {
   assert,
   assertEquals,
   assertNotEquals,
-} from "https://deno.land/std@0.145.0/testing/asserts.ts";
+} from "https://deno.land/std@0.146.0/testing/asserts.ts";
 
-export { green, red } from "https://deno.land/std@0.145.0/fmt/colors.ts";
-export { delay } from "https://deno.land/std@0.145.0/async/delay.ts";
+export { green, red } from "https://deno.land/std@0.146.0/fmt/colors.ts";
+export { delay } from "https://deno.land/std@0.146.0/async/delay.ts";
