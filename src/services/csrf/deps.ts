61d0c9f12daa883d7699f5f4fe4ff2b0fbe28fa4
drashbot <59404681+drashbot@users.noreply.github.com>
2022-07-06 00:45:28 +0000

    chore: update deps

--- src/services/csrf/deps.ts
+++ src/services/csrf/deps.ts
@@ -1,2 +1,2 @@
-export { createHash } from "https://deno.land/std@0.146.0/hash/mod.ts";
-export { v4 } from "https://deno.land/std@0.146.0/uuid/mod.ts";
+export { createHash } from "https://deno.land/std@0.147.0/hash/mod.ts";
+export { v4 } from "https://deno.land/std@0.147.0/uuid/mod.ts";
