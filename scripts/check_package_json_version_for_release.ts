a0a73e2bb25db57b372f17e5e1a3f75734fe4a23
Eric Crooks <crookse@users.noreply.github.com>
2023-10-17 06:36:49 -0400

    fix(ci): release scripts

---
+++ scripts/check_package_json_version_for_release.ts
@@ -0,0 +1,31 @@
+const versionToPublish = Deno.args[0];
+
+const packageJsonContents = new TextDecoder().decode(
+  Deno.readFileSync("./package.json"),
+);
+
+const packageJson = JSON.parse(packageJsonContents);
+const packageJsonVersion = `v${packageJson.version}`;
+
+console.log("\n/////////////////////////////////////////\n")
+console.log("\n//////////// MOMENT OF TRUTH ////////////\n")
+console.log("\n/////////////////////////////////////////\n")
+
+console.log("Checking package.json version with GitHub release tag version ...\n");
+
+console.log(`package.json version:   ${packageJsonVersion}`);
+console.log(`GitHub release version: ${versionToPublish}\n`);
+
+if (packageJsonVersion !== versionToPublish) {
+  console.log("!! Version mismatch !!");
+  console.log("!! Version mismatch !!");
+  console.log("!! Version mismatch !!");
+  console.log("\n\nStopping release process\n\n");
+  console.log("!! Version mismatch !!");
+  console.log("!! Version mismatch !!");
+  console.log("!! Version mismatch !!");
+  Deno.exit(1);
+} else {
+  console.log("Versions match")
+  console.log("\n\nProceeding with release workflow\n\n");
+}
